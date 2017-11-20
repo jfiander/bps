95930caf2a3a22c9b931e754b16bd0c141f8df23
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-20 00:50:37 -0500

    Add permission management code to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+  def ssl_configured?
+    Rails.env.production? || Rails.env.staging?
+  end
+
+  def require_permission(role)
+    redirect_to root_path and return unless current_user&.permitted?(role)
+    # before_action only: [:method_1, :method_2] { require_permission(:role_name) }
+  end
+
+  def current_user_is_admin?
+    user_signed_in? && current_user.permitted?(:admin)
+  end
 end
