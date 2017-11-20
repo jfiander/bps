0b095739541e7722bb002c123f419284a7f977a8
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-20 00:43:20 -0500

    Remove user_roles with owners

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ApplicationRecord
-  has_many :user_roles
+  has_many :user_roles, dependent: :destroy
   has_many :users, through: :user_roles
   belongs_to :parent, class_name: "Role", optional: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :invitable, :registerable, :database_authenticatable, :recoverable, :trackable, :validatable, :timeoutable, :lockable
-  has_many :user_roles
+  has_many :user_roles, dependent: :destroy
   has_many :roles, through: :user_roles
 
   validates_inclusion_of :grade, in: %w( S P AP JN N SN ), message: "must be one of [S, P, AP, JN, N, SN]"
