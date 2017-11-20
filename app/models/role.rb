349ad0a399a9c97b268048b639599be48819a843
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-20 00:07:03 -0500

    Make Role parent optional

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ApplicationRecord
   has_many :user_roles
   has_many :users, through: :user_roles
-  belongs_to :parent, class_name: "Role"
+  belongs_to :parent, class_name: "Role", optional: true
 end
