a897a5599aec7ae5f4090b00bd32709a8dfd7eaf
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-20 11:15:51 -0500

    Split MainController into two separate controllers

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,12 @@
+class MembersController < ApplicationController
+  before_action :authenticate_user!
+  before_action only: [:admin] { require_permission(:admin) }
+
+  def index
+    #
+  end
+
+  def admin
+    #
+  end
+end
---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,62 @@
+class PublicController < ApplicationController
+  def index
+    #
+  end
+
+  def about
+    #
+  end
+
+  def join
+    #
+  end
+
+  def vsc
+    #
+  end
+
+  def education
+    #
+  end
+
+  def calendar
+    #
+  end
+
+  def events
+    #
+  end
+
+  def photos
+    #
+  end
+
+  def civic
+    #
+  end
+
+  def bridge
+    #
+  end
+
+  def history
+    #
+  end
+
+  def newsletter
+    #
+  end
+
+  def store
+    #
+  end
+
+  def links
+    #
+  end
+
+  private
+  def clean_params
+    params.permit()
+  end
+end
