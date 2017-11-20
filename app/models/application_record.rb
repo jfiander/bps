938c18f4ec5b39e274a13c811e8ae883c04a9ea4
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-20 00:29:28 -0500

    Add find_or_create to ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.find_or_create(attributes)
+    query = self.find_by(attributes)
+    return self.create!(attributes) if query.blank?
+    query
+  end
 end
