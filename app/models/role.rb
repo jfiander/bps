03200c803e9ac1bb79022f395e2a655596ef0b6b
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-20 00:29:42 -0500

    Add association group methods to Role

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,20 @@ class Role < ApplicationRecord
   has_many :user_roles
   has_many :users, through: :user_roles
   belongs_to :parent, class_name: "Role", optional: true
+
+  def parents
+    parent = self.parent
+    parents_array = []
+    while parent.present?
+      parents_array << parent
+      parent = parent.parent
+    end
+    parents_array
+  end
+
+  def children
+    child_roles  = Role.where(parent_id: self.id).to_a
+    child_roles << child_roles.map(&:children) if child_roles.present?
+    child_roles.flatten
+  end
 end
