c2ba15ffe8330796b505e82f0eb4b58e2c6b510a
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-20 11:21:52 -0500

    Add routes for full menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
-  root                'main#index'
+  root                   'public#index'
 
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout'}
 
-  get '/members', to: 'main#members'
+  get '/about',      to: 'public#about'
+  get '/join',       to: 'public#join'
+  get '/vsc',        to: 'public#vsc'
+  get '/education',  to: 'public#education'
+  get '/calendar',   to: 'public#calendar'
+  get '/events',     to: 'public#events'
+  get '/photos',     to: 'public#photos'
+  get '/civic',      to: 'public#civic'
+  get '/bridge',     to: 'public#bridge'
+  get '/history',    to: 'public#history'
+  get '/newsletter', to: 'public#newsletter'
+  get '/store',      to: 'public#store'
+  get '/links',      to: 'public#links'
 
-  get '/admin', to: 'main#admin'
+  get '/members',    to: 'members#index'
+  get '/admin',      to: 'members#admin'
 
-  get '/*path',   to: redirect('/') unless Rails.env.development?
+  get '/*path',      to: redirect('/') unless Rails.env.development?
 end
