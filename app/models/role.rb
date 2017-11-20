7ada6985a3527a043af2195375201fd766a9be4d
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-19 23:47:39 -0500

    Add permissions models

---
+++ app/models/role.rb
@@ -0,0 +1,5 @@
+class Role < ApplicationRecord
+  has_many :user_roles
+  has_many :users, through: :user_roles
+  belongs_to :parent, class_name: "Role"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120040643) do
+ActiveRecord::Schema.define(version: 20171120042550) do
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "certificate"
