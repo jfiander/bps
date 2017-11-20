374f4ffbf49cab296852e3abb56b1bffe8c6bf3b
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-20 00:29:49 -0500

    Add permission methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,26 @@ class User < ApplicationRecord
   has_many :roles, through: :user_roles
 
   validates_inclusion_of :grade, in: %w( S P AP JN N SN ), message: "must be one of [S, P, AP, JN, N, SN]"
+
+  def permitted?(role)
+    role = Role.find_by(name: role.to_s)
+    return false if role.blank?
+
+    # True if user's roles include the specified role or any parent role
+    role.in?(self.roles) || role.parents.any? { |r| r.in? self.roles }
+  end
+
+  def permitted_roles
+    self.roles.map(&:children).flatten.map(&:name).map(&:to_sym)
+  end
+
+  def permit!(role)
+    role = Role.find_by(name: role.to_s)
+    UserRole.find_or_create(user: self, role: role)
+  end
+
+  def unpermit!(role)
+    UserRole.where(user: self).destroy_all and return true if role == :all
+    UserRole.where(user: self, role: Role.find_by(name: role.to_s)).destroy_all.present?
+  end
 end
