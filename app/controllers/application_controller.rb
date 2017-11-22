640d4a79cd6b46f8c78f0676fdf3596c38efe161
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-21 23:12:44 -0500

    Remove unnecessary admin check method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,13 @@ class ApplicationController < ActionController::Base
   force_ssl if: :ssl_configured?
   protect_from_forgery with: :exception
 
-  before_action :display_admin_menu, if: :current_user_is_admin?
-
   private
   def ssl_configured?
     Rails.env.production? || Rails.env.staging?
   end
 
-  def display_admin_menu
-    @admin_menu = true
-  end
-
   def require_permission(role)
     redirect_to root_path and return unless current_user&.permitted?(role)
     # before_action only: [:method_1, :method_2] { require_permission(:role_name) }
-  end
-
-  def current_user_is_admin?
-    user_signed_in? && current_user.permitted?(:admin)
   end
 end
