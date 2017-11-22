71cf12ab7c952006cad7131215dcf0faf8511766
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-22 14:45:07 -0500

    Add remove Bilge functionality

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,22 +1,36 @@
 class MembersController < ApplicationController
   before_action :authenticate_user!
-  before_action only: [:admin] { require_permission(:admin) }
-  before_action only: [:upload_bilge] { require_permission(:newsletter) }
+  before_action                   only: [:admin] { require_permission(:admin) }
+  before_action                   only: [:upload_bilge] { require_permission(:newsletter) }
+  before_action :get_bilge_issue, only: [:upload_bilge, :remove_bilge]
 
   def index
     #
   end
 
   def upload_bilge
-    year = clean_params[:issue]['date(1i)']
-    month = clean_params[:issue]['date(2i)']
-    month = "s" if month.to_i.in? [7,8]
-    BpsS3.upload(clean_params[:bilge_upload_file], bucket: :bilge, key: "#{year}/#{month}.pdf")
-    redirect_to bilge_path, notice: "Bilge Chatter uploaded successfully."
+    redirect_to newsletter_path, alert: "You must either upload a file or check the remove box." and return unless clean_params[:bilge_upload_file] || clean_params[:bilge_remove]
+    remove_bilge and return if clean_params[:bilge_remove].present?
+
+    BpsS3.upload(clean_params[:bilge_upload_file], bucket: :bilge, key: @key)
+    redirect_to newsletter_path, notice: "Bilge Chatter uploaded successfully."
   end
 
   private
   def clean_params
-    params.permit(:bilge_upload_file, issue: ['date(1i)', 'date(2i)'])
+    params.permit(:bilge_upload_file, :bilge_remove, issue: ['date(1i)', 'date(2i)'])
+  end
+
+  def get_bilge_issue
+    @year = clean_params[:issue]['date(1i)']
+    month = clean_params[:issue]['date(2i)']
+    @month = month.to_i.in?([7,8]) ? "s" : month
+    @issue = "#{@year}/#{@month}"
+    @key = "#{@issue}.pdf"
+  end
+
+  def remove_bilge
+    BpsS3.remove_object(bucket: :bilge, key: @key)
+    redirect_to newsletter_path, notice: "Bilge Chatter #{@issue} removed successfully."
   end
 end
