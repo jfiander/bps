43d96a360445a21dddfee570e2020faffffa0ba1
Julian Fiander <julianfiander@gmail.com>
2017-11-23 20:10:20 -0500

    Add sender name to devise config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'support@bpsd9.org'
+  config.mailer_sender = '"BPS Support" <support@bpsd9.org>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
