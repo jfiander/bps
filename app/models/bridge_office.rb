d39392fb462aac98cc8f06027380e2920291cd78
Julian Fiander <julianfiander@gmail.com>
2017-11-24 09:12:33 -0500

    Clean up BridgeOffice ordered scope to use heredoc

--- app/models/bridge_office.rb
+++ app/models/bridge_office.rb
@@ -14,16 +14,18 @@ class BridgeOffice < ApplicationRecord
   scope :heads,      -> { where.not("office LIKE ?", "asst_%") }
   scope :assistants, -> { where("office LIKE ?", "asst_%") }
   scope :ordered,    -> {
-    order("CASE
-      WHEN office = 'commander'        THEN '1'
-      WHEN office = 'executive'        THEN '2'
-      WHEN office = 'administrative'   THEN '3'
-      WHEN office = 'educational'      THEN '4'
-      WHEN office = 'secretary'        THEN '5'
-      WHEN office = 'treasurer'        THEN '6'
-      WHEN office = 'asst_educational' THEN '7'
-      WHEN office = 'asst_secretary'   THEN '8'
-    END")
+    order <<~SQL
+      CASE
+        WHEN office = 'commander'        THEN '1'
+        WHEN office = 'executive'        THEN '2'
+        WHEN office = 'administrative'   THEN '3'
+        WHEN office = 'educational'      THEN '4'
+        WHEN office = 'secretary'        THEN '5'
+        WHEN office = 'treasurer'        THEN '6'
+        WHEN office = 'asst_educational' THEN '7'
+        WHEN office = 'asst_secretary'   THEN '8'
+      END
+    SQL
   }
 
   def title
