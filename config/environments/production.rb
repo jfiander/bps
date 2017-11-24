d785e7b72bef3e92a7bd6407ca153d765404b51b
Julian Fiander <julianfiander@gmail.com>
2017-11-23 19:43:52 -0500

    Add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'bpsd9.org', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://bpsd9.herokuapp.com' }
+
+  ActionMailer::Base.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => 'bpsd9.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
