1ac99ff8dc617d600dbe4a4e0a129669d57a07e2
Julian Fiander <julianfiander@gmail.com>
2017-11-24 10:15:54 -0500

    Support profile photo upload

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  #after_save { upload_profile_photo }
+  def update
+    super
+    BpsS3.upload(update_params[:profile_photo], bucket: :files, key: "profile_photos/#{self.resource.certificate}.jpg")
+  end
+
+  def update_params
+    params.require(:user).permit(:profile_photo, :first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
