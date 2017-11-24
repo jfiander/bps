db4630ef8e7e5e324346ec61b9aa79ddb13dba31
Julian Fiander <julianfiander@gmail.com>
2017-11-24 16:40:21 -0500

    Add validation on Committee name

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -1,6 +1,7 @@
 class Committee < ApplicationRecord
   belongs_to :chair, class_name: "User", optional: true
 
+  validates :name, uniqueness: { scope: :department }
   validates :department, inclusion: { in: %w[commander executive educational
     administrative secretary treasurer asst_educational asst_secretary],
     message: "%{value} is not a valid department" }
