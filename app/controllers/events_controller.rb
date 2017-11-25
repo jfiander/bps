0cd8c24b1816e24fc42bc47b21aae5797324b821
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-25 12:55:52 -0500

    Fix submit_paths

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,18 +6,18 @@ class EventsController < ApplicationController
 
   def new_course
     @event = Event.new(event_category: EventCategory.find_by(title: "advanced_grade"))
-    @submit_path = "create_course"
+    @submit_path = create_course_path
     @course_types = EventType.where(event_category: EventCategory.courses).map(&:title).map(&:titleize)
   end
 
   def new_seminar
     @event = Event.new(event_type: EventType.find_by(title: "seminar"))
-    @submit_path = "create_seminar"
+    @submit_path = create_seminar_path
   end
 
   def new_meeting
     @event = Event.new(event_type: EventType.find_by(title: "meeting"))
-    @submit_path = "create_meeting"
+    @submit_path = create_meeting_path
   end
 
   def create_course
