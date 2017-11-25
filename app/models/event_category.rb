fa10ded31e0f427f79d3503fb386b00a6cd41db9
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-25 12:11:56 -0500

    Add scopes on EventCategory

--- app/models/event_category.rb
+++ app/models/event_category.rb
@@ -1,4 +1,8 @@
 class EventCategory < ApplicationRecord
   has_many :event_types
   has_many :events
+
+  scope :courses,  -> { where(title: ["advanced_grade", "elective"]) }
+  scope :seminars, -> { where(title: ["seminar"]) }
+  scope :events,   -> { where(title: ["meeting"]) }
 end
