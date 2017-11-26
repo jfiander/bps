144b8d009a393a382c471d5831ee64e3b01b3008
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-25 19:08:30 -0500

    Add EventType#display_title

--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -1,4 +1,18 @@
 class EventType < ApplicationRecord
   belongs_to :event_category
   has_many   :events
+
+  def display_title
+    title.titleize.
+      gsub(/gps/i, "GPS").
+      gsub(/vhf/i, "VHF").
+      gsub(/dsc/i, "DSC").
+      gsub(/ A /, " a ").
+      gsub(/ To /, " to ").
+      gsub(/ The /, " the ").
+      gsub(/ Of /, " of ").
+      gsub(/ And /, " and ").
+      gsub(/ On /, " on ").
+      gsub(/ For /, " for ")
+  end
 end
