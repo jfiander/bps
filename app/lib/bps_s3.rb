308ac6958aef27144ef2efd27bf1c6f39739ce96
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-29 23:48:04 -0500

    Restructure BpsS3 as a class

--- app/lib/bps_s3.rb
+++ app/lib/bps_s3.rb
@@ -1,68 +1,58 @@
-module BpsS3
-  def self.in_bucket(bucket_type)
-    # Buckets:
-    #  bps-bilge
-    #  bps-files
-    #  bps-photos
+class BpsS3
+  # usage:  BpsS3.new { |b| b.bucket = :files }
+
+  attr_accessor :bucket
+  # bilge, files, photos
+
+  def initialize
+    @environment = ENV['ASSET_ENVIRONMENT']
+    yield self if block_given?
+
+    if @bucket == :static
+      @endpoint = @environment = :static
+      @bucket = :files
+    else
+      @endpoint = @bucket
+    end
+  end
+
+  def list(prefix: "")
+    s3.objects({prefix: prefix})
+  end
+
+  def object(key:)
+    s3.object(key)
+  end
+
+  def upload(file:, key:)
+    s3.object(key).upload_file(file.path)
+  end
+
+  def remove_object(key:)
+    s3.object(key).delete
+  end
+
+  def link(key:)
+    "https://#{cf_host}/#{key}"
+  end
+
+  def download(key:)
+    object(key: key).get.body.read
+  end
+
+  private
+  def full_bucket
+    "bps-#{@environment}-#{@bucket}"
+  end
+
+  def s3
     Aws::S3::Resource.new(
       region: 'us-east-2',
       credentials: Aws::Credentials.new(Rails.application.secrets[:s3_access_key], Rails.application.secrets[:s3_secret])
-    ).bucket("bps-#{bucket_type.to_s}")
+    ).bucket(full_bucket)
   end
 
-  def self.upload(file, bucket:, key:)
-    in_bucket(bucket).object(key).upload_file(file.path) #, acl: 'public-read'
-  end
-
-  def self.get_object(bucket:, key:)
-    in_bucket(bucket).object(key)
-  end
-
-  def self.link(bucket:, key:)
-    object = get_object(bucket: bucket, key: key)
-    if object.exists?
-      object.presigned_url(
-        :get,
-        expires_in: 20.minutes,
-        response_content_disposition: "attachment;filename=Bilge_Chatter_#{key.gsub('/', '-')}"
-      ).to_s
-    end
-  end
-
-  def self.download(bucket:, key:)
-    get_object(bucket: bucket, key: key).get.body.string
-  end
-
-  def self.download_with_prefix(bucket:, prefix:, to:)
-    in_bucket(bucket).objects({prefix: prefix}).each do |obj|
-      outfile = "#{to}/#{obj.key}".sub(prefix, '')
-      next if File.directory?(outfile)
-      outdir = outfile.dup.split("/")
-      outdir.pop
-      FileUtils.mkdir_p(outdir.join("/"))
-      obj.get(response_target: outfile)
-    end
-  end
-
-  def self.list(bucket:, prefix: "")
-    in_bucket(bucket).objects({prefix: prefix})
-  end
-
-  def self.remove_object(bucket:, key:)
-    in_bucket(bucket).object(key).delete
-  end
-
-  module CloudFront
-    def self.host(bucket)
-      {
-        files: ENV['CLOUDFRONT_FILES_ENDPOINT'],
-        bilge: ENV['CLOUDFRONT_BILGE_ENDPOINT'],
-        photos: ENV['CLOUDFRONT_PHOTOS_ENDPOINT'],
-      }[bucket]
-    end
-
-    def self.link(bucket:, key:)
-      "https://#{host(bucket)}/#{key}"
-    end
+  def cf_host
+    ENV["CLOUDFRONT_#{@endpoint.upcase}_ENDPOINT"]
   end
 end
