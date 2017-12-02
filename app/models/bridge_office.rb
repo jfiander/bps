535a2bdc19ddf91b281f7f24f430ac3fc072492e
Julian Fiander <jsfiander@gmail.com>
2017-12-02 15:10:41 -0500

    Adjust order of bridge offices

--- app/models/bridge_office.rb
+++ app/models/bridge_office.rb
@@ -18,8 +18,8 @@ class BridgeOffice < ApplicationRecord
       CASE
         WHEN office = 'commander'        THEN '1'
         WHEN office = 'executive'        THEN '2'
-        WHEN office = 'administrative'   THEN '3'
-        WHEN office = 'educational'      THEN '4'
+        WHEN office = 'educational'      THEN '3'
+        WHEN office = 'administrative'   THEN '4'
         WHEN office = 'secretary'        THEN '5'
         WHEN office = 'treasurer'        THEN '6'
         WHEN office = 'asst_educational' THEN '7'
