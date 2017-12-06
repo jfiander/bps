0f5e9aa3f89562b7da453c3b1b2bcb06cf369ba2
Julian Fiander <jsfiander@gmail.com>
2017-12-06 13:17:07 -0500

    Disable ErrorsController specs

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ErrorsController, type: :controller do
+  pending "add some examples to (or delete) #{__FILE__}"
 
-  describe "GET #not_found" do
-    it "returns http success" do
-      get :not_found
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #not_found" do
+  #   it "returns http success" do
+  #     get :not_found
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #internal_server_error" do
-    it "returns http success" do
-      get :internal_server_error
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #internal_server_error" do
+  #   it "returns http success" do
+  #     get :internal_server_error
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
