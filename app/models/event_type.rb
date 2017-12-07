3576da2265b4118f0d1f1a4972bae8d1c7376e67
Julian Fiander <jsfiander@gmail.com>
2017-12-07 12:38:13 -0500

    Add ordered scope to Event

--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -15,6 +15,24 @@ class EventType < ApplicationRecord
   scope :seminar,         -> { seminars }
   scope :meeting,         -> { meetings }
 
+  scope :ordered,         -> {
+    order <<~SQL
+      CASE
+        WHEN event_category = 'public'     THEN '1' || title
+        WHEN event_category = 'advanced_grade' THEN CASE
+          WHEN title = 'seamanship'        THEN '2'
+          WHEN title = 'piloting'          THEN '3'
+          WHEN title = 'advanced_piloting' THEN '4'
+          WHEN title = 'junior_navigation' THEN '5'
+          WHEN title = 'navigation'        THEN '6'
+        END
+        WHEN event_category = 'elective'   THEN '7' || title
+        WHEN event_category = 'seminar'    THEN '8' || title
+        WHEN event_category = 'meeting'    THEN '9' || title
+      END
+    SQL
+  }
+  
   def display_title
     cleanup_titles(title.titleize)
   end
