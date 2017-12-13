e463a3324362960c4d85a4801cf769f3070396a0
Julian Fiander <jsfiander@gmail.com>
2017-12-13 10:02:54 -0500

    Disable validation on Registration

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,7 +2,7 @@ class Registration < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :event
 
-  validates :event, uniqueness: {scope: :user}
+  # validates :event, uniqueness: {scope: :user}
 
   scope :current,  -> { all.find_all { |r| r.event.expires_at.future? } }
   scope :expired,  -> { all.find_all { |r| r.event.expires_at.past? } }
