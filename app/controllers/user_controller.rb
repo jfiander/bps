f2ff77ce754c7b0ee9a549cf6e7bb2fa1e5ce513
Julian Fiander <jsfiander@gmail.com>
2017-12-21 12:02:23 -0500

    Prevent invite_all from running anywhere but production

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -211,6 +211,8 @@ class UserController < ApplicationController
   end
 
   def invite_all
+    redirect_to users_path, alert: "This action is only available on Production." and return unless ENV["ASSET_ENVIRONMENT"] == "production"
+
     users = User.all.reject { |u| u.sign_in_count > 0 || u.email.match(/@example\.com/) || u.email.match(/bpsd9\.org/) }
     users.each { |user| user.invite! }
     redirect_to users_path, notice: "All new users have been sent invitations."
