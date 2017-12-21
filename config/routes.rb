c718b1cb6f955821e15c91fedaa8613011238696
Julian Fiander <jsfiander@gmail.com>
2017-12-21 10:52:50 -0500

    Redirect to inivitation page after invitation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, path: '',
     path_names: {sign_in: 'login', sign_out: 'logout'},
-    controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
+    controllers: {registrations: 'users/registrations', sessions: 'users/sessions', invitations: 'users/invitations'}
 
   # Profile management
   as :user do
