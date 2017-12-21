6a4c9c7519eab15eb8ec70118cc81fa1d4012791
Julian Fiander <jsfiander@gmail.com>
2017-12-21 12:53:46 -0500

    Fix invitations redirect

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,5 +1,5 @@
 class Users::InvitationsController < Devise::InvitationsController
-  before_action :redirect_if_no_invitations
+  before_action :redirect_if_no_invitations, except: [:edit]
 
   def after_invite_path_for(resource)
     new_user_invitation_path
@@ -7,6 +7,6 @@ class Users::InvitationsController < Devise::InvitationsController
 
   private
   def redirect_if_no_invitations
-    redirect_to root_path, alert: "You do not have any invitations remaining." and return unless current_user&.invitation_limit > 0
+    redirect_to root_path, alert: "You do not have any invitations remaining." and return if current_user&.invitation_limit.in?([nil, 0])
   end
 end
