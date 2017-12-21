0d7a2444b7680198c48ed1d90693eb1ce7538b26
Julian Fiander <jsfiander@gmail.com>
2017-12-21 12:29:54 -0500

    Redirect from all invitations methods unless user has invitations remaining

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,5 +1,12 @@
 class Users::InvitationsController < Devise::InvitationsController
+  before_action :redirect_if_no_invitations
+
   def after_invite_path_for(resource)
     new_user_invitation_path
+  end
+
+  private
+  def redirect_if_no_invitations
+    redirect_to root_path, alert: "You do not have any invitations remaining." and return unless current_user&.invitation_limit > 0
   end
 end
