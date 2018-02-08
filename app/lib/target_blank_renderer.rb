a0001e341d719d5cf3ea054d369974ce88915365
Julian Fiander <jsfiander@gmail.com>
2018-02-08 16:50:25 -0500

    Remove target blank from local links in markdown

--- app/lib/target_blank_renderer.rb
+++ app/lib/target_blank_renderer.rb
@@ -1,5 +1,17 @@
 class TargetBlankRenderer < Redcarpet::Render::HTML
-  def initialize(extensions = {})
-    super extensions.merge(link_attributes: { target: "_blank" })
+  def link(link, title, alt_text)
+    if link.match(/^https?:\/\/#{ENV["DOMAIN"]}\/.*/) || link.match(/^\//)
+      "<a href='#{link}'>#{alt_text}</a>"
+    else
+      "<a target='_blank' href='#{link}'>#{alt_text}</a>"
+    end
+  end
+
+  def autolink(link, link_type)
+    if link.match(/^https?:\/\/#{ENV["DOMAIN"]}\/.*/) || link.match(/^\//)
+      "<a href='#{link}'>#{link}</a>"
+    else
+      "<a target='_blank' href='#{link}'>#{link}</a>"
+    end
   end
 end
