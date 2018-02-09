525c7155cb3faface73f1cced62ce2e0bad8d122
Julian Fiander <julianfiander@gmail.com>
2018-02-09 16:39:56 -0500

    Fix validations on registrations

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,9 +2,18 @@ class Registration < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :event
 
-  # validates :event, uniqueness: {scope: :user}
+  validate :email_or_user_present, :no_duplicate_registrations
 
   scope :current,  -> { all.find_all { |r| r.event.expires_at.future? } }
   scope :expired,  -> { all.find_all { |r| r.event.expires_at.past? } }
   scope :for_user, ->(user_id) { where(user_id: user_id) }
+
+  private
+  def email_or_user_present
+    errors.add(:base, 'Must have a user or event') unless user.present? || email.present?
+  end
+
+  def no_duplicate_registrations
+    errors.add(:base, 'Duplicate') unless Registration.where(user: user, email: email, event: event).where.not(id: id).blank?
+  end
 end
