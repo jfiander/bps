589a78a6a78c06e5725b0ee83584599a97137970
Julian Fiander <jsfiander@gmail.com>
2018-02-11 16:13:09 -0500

    Clean up ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   def require_permission(*role)
     redirect_to root_path and return unless current_user&.permitted?(*role)
-    # before_action only: [:method_1, :method_2] { require_permission(:role_name) }
+    # before_action only: [:method_1] { require_permission(:role_name) }
   end
 
   def authenticate_inviter!
@@ -28,28 +28,59 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:profile_photo, :rank, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:profile_photo, :rank, :first_name, :last_name]
+    )
   end
 
   def time_formats
-    @long_time_format = "%a %d %b %Y @ %H%M %Z"
-    @medium_time_format = "%-m/%-d/%Y @ %H%M"
-    @short_time_format = "%-m/%-d @ %H%M"
-    @duration_format = "%-kh %Mm"
+    @long_time_format = '%a %d %b %Y @ %H%M %Z'
+    @medium_time_format = '%-m/%-d/%Y @ %H%M'
+    @short_time_format = '%-m/%-d @ %H%M'
+    @duration_format = '%-kh %Mm'
   end
 
   def render_markdown
-    render layout: "application", inline: render_markdown_raw(name: action_name)
+    render layout: 'application', inline: render_markdown_raw(name: action_name)
   end
 
   def render_markdown_raw(name: nil, markdown: nil)
-    raise ArgumentError, "Must provide name or markdown." unless name.present? || markdown.present?
+    unless name.present? || markdown.present?
+      raise ArgumentError, 'Must provide name or markdown.'
+    end
 
-    markdown ||= StaticPage.find_by(name: name)&.markdown
-    burgee_html = center_html { USPSFlags::Burgees.new { |b| b.squadron = :birmingham }.svg }
-    education_menu = view_context.render "application/education_menu", active: {courses: false, seminars: false}
+    @page_markdown = markdown
 
-    ("<div class='markdown'>" + Redcarpet::Markdown.new(TargetBlankRenderer,
+    preload_markdown(name)
+    generate_markdown_div
+    parse_markdown_div
+  end
+
+  def preload_markdown(name)
+    @page_markdown ||= StaticPage.find_by(name: name)&.markdown
+
+    @burgee_html = center_html do
+      USPSFlags::Burgees.new { |b| b.squadron = :birmingham }.svg
+    end
+
+    @education_menu = view_context.render(
+      'application/education_menu',
+      active: { courses: false, seminars: false }
+    )
+  end
+
+  def generate_markdown_div
+    @markdown_div = '<div class="markdown">'
+    @markdown_div << redcarpet.render(@page_markdown.to_s.gsub(/(#+)/, '#\1'))
+    @markdown_div << '</div>'
+
+    @markdown_div
+  end
+
+  def redcarpet
+    Redcarpet::Markdown.new(
+      TargetBlankRenderer,
       autolink: true,
       images: true,
       tables: true,
@@ -57,50 +88,52 @@ class ApplicationController < ActionController::Base
       strikethrough: true,
       superscript: true,
       underline: true
-    ).render(markdown.to_s.
-      gsub(/(#+)/, '#\1')
-    ) + "</div>").
-      gsub("<p>@", '<p class="center">').
-      gsub(/<p>%burgee<\/p>/, burgee_html).
-      gsub(/<p>%education<\/p>/, education_menu).
-      gsub(/(.*?)%static_file\/(.*?)\/(.*?)\/(.*?)$/) { $1 + markdown_static_link($2, title: $3) + $4 }.
-      gsub(/(.*?)%file\/(\d+)\/(.*?)\/(.*?)$/) { $1 + markdown_file_link($2, title: $3) + $4 }.
-      gsub(/(.*?)%image\/(\d+)\/(.*?)$/) { $1 + markdown_image($2) + $3 }.
-      gsub("&reg;", "<sup>&reg;</sup>").
-      gsub("<ul>", "<ul class='md'>").
-      gsub(/(.*?)%fa\/(.*?)\/(.*?)$/) { $1 + view_context.fa_icon($2) + $3 }
+    )
+  end
+
+  def parse_markdown_div
+    @markdown_div
+      .gsub('<p>@', '<p class="center">')
+      .gsub(%r{<p>%burgee</p>}, @burgee_html)
+      .gsub(%r{<p>%education</p>}, @education_menu)
+      .gsub(%r{(.*?)%static_file/(.*?)/(.*?)/(.*?)$}) { $1 + markdown_static_link($2, title: $3) + $4 }
+      .gsub(%r{(.*?)%file/(\d+)/(.*?)/(.*?)$}) { $1 + markdown_file_link($2, title: $3) + $4 }
+      .gsub(%r{(.*?)%image/(\d+)/(.*?)$}) { $1 + markdown_image($2) + $3 }
+      .gsub('&reg;', '<sup>&reg;</sup>')
+      .gsub('<ul>', '<ul class="md">')
+      .gsub(%r{(.*?)%fa/(.*?)/(.*?)$}) { $1 + view_context.fa_icon($2) + $3 }
   end
 
   def center_html
-    "<div class='center'>" + yield + "</div>"
+    '<div class="center">' + yield + '</div>'
   end
 
   def pick_header_image
-    objects = static_bucket.list(prefix: "headers/")
+    objects = static_bucket.list(prefix: 'headers/')
     keys = objects.map(&:key)
     keys.shift
     @header_image = static_bucket.link(key: keys.sample)
-    @header_logo = static_bucket.link(key: "logos/ABC.tr.300.png")
-    @print_logo = static_bucket.link(key: "logos/ABC.long.birmingham.1000.png")
-    @wheel_logo = static_bucket.link(key: "flags/PNG/WHEEL.thumb.png")
-    @dca_award = static_bucket.link(key: "logos/DCA_web_2016.png")
+    @header_logo = static_bucket.link(key: 'logos/ABC.tr.300.png')
+    @print_logo = static_bucket.link(key: 'logos/ABC.long.birmingham.1000.png')
+    @wheel_logo = static_bucket.link(key: 'flags/PNG/WHEEL.thumb.png')
+    @dca_award = static_bucket.link(key: 'logos/DCA_web_2016.png')
   end
 
-  def markdown_static_link(id, title: "")
+  def markdown_static_link(id, title: '')
     key = get_uploaded_file_name(id)
     link_title = title || key
     link_path = static_bucket.link(key: "general/#{key}")
     view_context.link_to(link_path, target: :_blank) do
-      view_context.fa_icon("cloud-download") + link_title
+      view_context.fa_icon('cloud-download') + link_title
     end
   end
 
-  def markdown_file_link(id, title: "")
+  def markdown_file_link(id, title: '')
     key = get_uploaded_file_name(id)
     link_title = title || key
     link_path = files_bucket.link(key: "uploaded_files/#{key}")
     view_context.link_to(link_path, target: :_blank) do
-      view_context.fa_icon("cloud-download") + link_title
+      view_context.fa_icon('cloud-download') + link_title
     end
   end
 
@@ -120,7 +153,7 @@ class ApplicationController < ActionController::Base
       Join the Birmingham chapter of the United States Power Squadrons®.
     DESCRIPTION
 
-    keywords = <<~KEYWORDS
+    @site_keywords = <<~KEYWORDS
       America's Boating Club®, America's Boating Club, birminghampowersquadron.org, Power Squadron, bpsd9.org, Michigan,
       Boating Safety, D9, District 9, fun boating, boating, Birmingham, boater, water, recreation,
       recreational, safety, vessels, education, boating classes, join, weather, sail, sailing,
@@ -129,6 +162,5 @@ class ApplicationController < ActionController::Base
       Power Squadron, United States Power Squadrons, USPS, Safe Boating, VSC, Vessel Exams,
       Vessel Safety Checks, vessel safety
     KEYWORDS
-    @site_keywords = keywords
   end
 end
