11c563576e1e3ad1f2e340e706c74a13e44febec
Julian Fiander <jsfiander@gmail.com>
2018-02-20 11:28:08 -0500

    Apply big button style

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -1,12 +1,12 @@
 module ViewHelper
   def officer_flag(office, mode: :svg)
     rank = case office
-    when "commander"
-      "CDR"
-    when "executive", "educational", "administrative", "secretary", "treasurer"
-      "LTC"
-    when "asst_educational", "asst_secretary"
-      "1LT"
+    when 'commander'
+      'CDR'
+    when 'executive', 'educational', 'administrative', 'secretary', 'treasurer'
+      'LTC'
+    when 'asst_educational', 'asst_secretary'
+      '1LT'
     end
 
     if mode == :svg
@@ -16,11 +16,11 @@ module ViewHelper
     end
   end
 
-  def spinner_button(form = nil, button_text: "Submit", disable_text: nil, name: "button")
-    disable_text ||= button_text == "Submit" ? "Submitting" : button_text.sub(/e$/, '') + "ing"
-    data_hash = { disable_with: (fa_icon("spinner pulse") + "#{disable_text}...") }
+  def spinner_button(form = nil, button_text: 'Submit', disable_text: nil, name: 'button', css_class: '')
+    disable_text ||= button_text == 'Submit' ? 'Submitting' : button_text.sub(/e$/, '') + 'ing'
+    data_hash = { disable_with: (fa_icon('spinner pulse') + "#{disable_text}...") }
 
-    return form.button(button_text, data: data_hash, name: name) if form.present?
-    button_tag(button_text, data: data_hash, name: name)
+    return form.button(button_text, data: data_hash, name: name, class: css_class) if form.present?
+    button_tag(button_text, data: data_hash, name: name, class: css_class)
   end
 end
