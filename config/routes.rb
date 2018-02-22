a59b45a9ce77aa8863be23d856650e492c6fbfed
Julian Fiander <jsfiander@gmail.com>
2018-02-21 20:06:28 -0500

    Add redirect route for profile

--- config/routes.rb
+++ config/routes.rb
@@ -86,6 +86,7 @@ Rails.application.routes.draw do
   end
 
   # User management
+  get     '/profile',                       to: redirect('/users/current')
   get     '/users',                         to: 'user#list'
   get     '/users/current',                 to: 'user#current',                   as: 'current_user'
   get     '/users/:id',                     to: 'user#show',                      as: 'user'
