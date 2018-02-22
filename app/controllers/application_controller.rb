4bd5af6e469e4cabe6efd3aaf1078e3081d70f49
Julian Fiander <jsfiander@gmail.com>
2018-02-22 11:19:03 -0500

    Add login referral

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,11 @@ class ApplicationController < ActionController::Base
     # before_action only: [:method_1] { require_permission(:role_name) }
   end
 
+  def authenticate_user!
+    return super if user_signed_in?
+    redirect_to new_user_session_path, flash: { referrer: request.original_fullpath }
+  end
+
   def authenticate_inviter!
     redirect_to root_path and return unless current_user&.permitted?(:users)
     super
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,18 @@
 class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
-      resource.sign_in_count == 1 ? welcome_path : members_path
+    return welcome_path if resource.sign_in_count == 1
+    return referrer_params[:user][:referrer] if useful_referrer?
+    members_path
+  end
+
+  private
+
+  def useful_referrer?
+    referrer_params[:user][:referrer].present? &&
+      referrer_params[:user][:referrer] != '/login'
+  end
+
+  def referrer_params
+    params.permit(user: :referrer)
   end
 end
