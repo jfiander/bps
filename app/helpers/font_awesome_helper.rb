495492fd8300577c2240efff5ac74d9e035b634d
Julian Fiander <jsfiander@gmail.com>
2018-02-23 11:52:07 -0500

    Improve FontAwesomeHelper

--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -1,8 +1,22 @@
+# FontAwesome 5 (Pro) Heler
 module FontAwesomeHelper
-  def fa_layer(icons = {}, title: nil)
+  def fa_layer(icons = {}, title: nil, grow: 0, css: '')
     # Add icons to the stack bottom to top
-    span_top = "<span class='icon fa-layers fa-fw' title='#{title}'>"
+    #
+    # Note: scaling ounters does not work well with :grow, so should use the
+    # older "fa-3x" syntax in :css instead.
+    span_top = "<span class='icon fa-layers fa-fw #{css}' title='#{title}'>"
     span_bottom = '</span>'
+
+    icons.each do |i|
+      combined_grow = if i.key?(:options) && i[:options].key?(:grow)
+                        i[:options][:grow] + grow
+                      else
+                        grow
+                      end
+
+      i[:options] = i[:options].merge(grow: combined_grow)
+    end
 
     output = span_top + parse_all(icons).join + span_bottom
     output.html_safe
@@ -40,17 +54,18 @@ module FontAwesomeHelper
       name = icon[:name]
       options = icon[:options] || {}
 
-      parse_icon(name, options)
+      if name.in? [:counter, :text]
+        parse_span(name, icon[:text], options)
+      else
+        parse_icon(name, options)
+      end
     end
   end
 
   def parse_icon(name, options = {})
-    options = default_icon_options.merge(options) rescue default_icon_options
-    parse_classes(options)
-    parse_transforms(options.slice(
-      :grow, :shrink, :rotate, :up, :down, :left, :right
-    ))
-    title = options[:title] if options.has_key?(:title)
+    options = fa_options(options)
+    parse_options(options)
+    title = options[:title]
 
     @classes << "fa-#{name}"
     css = @classes.flatten.join(' ')
@@ -60,21 +75,24 @@ module FontAwesomeHelper
   end
 
   def parse_span(type, text, options = {})
-    options = default_icon_options.merge(options) rescue default_icon_options
-    parse_classes(options)
-    parse_transforms(options.slice(
-      :grow, :shrink, :rotate, :up, :down, :left, :right
-    ))
+    options = fa_options(options)
+    parse_options(options)
 
     @classes << "fa-layers-#{type}"
-    css = @classes.join(' ')
+    css = @classes.flatten.reject { |c| c.match(/^fa.$/) }.join(' ')
     transforms = @transforms.join(' ')
 
     "<span class='#{css}' data-fa-transform='#{transforms}'>#{text}</span>"
   end
 
-  def default_icon_options
-    { style: :solid, css: '', fa: '', size: 1 }
+  def fa_options(options)
+    default = { style: :solid, css: '', fa: '' }
+
+    begin
+      default.merge(options)
+    rescue
+      default
+    end
   end
 
   def size_x(size)
@@ -82,8 +100,10 @@ module FontAwesomeHelper
     "-#{size}x"
   end
 
-  def parse_classes(options)
+  def parse_options(options)
     @classes = []
+    @transforms = []
+
     @classes << 'fa' + case options[:style]
                        when :solid
                          's'
@@ -96,10 +116,7 @@ module FontAwesomeHelper
                        end
     @classes << options[:fa].to_s.split(' ').map { |c| "fa-#{c}" }
     @classes << options[:css].to_s.split(' ')
-  end
 
-  def parse_transforms(options)
-    @transforms = []
     @transforms << "grow-#{options[:grow]}" if options[:grow].present?
     @transforms << "shrink-#{options[:shrink]}" if options[:shrink].present?
     @transforms << "rotate-#{options[:rotate]}" if options[:rotate].present?
