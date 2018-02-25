fa1fcd634db62f7fdf174286910002cf83a206ba
Julian Fiander <julianfiander@gmail.com>
2018-02-24 19:54:26 -0500

    Whitelist login redirect paths

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     return welcome_path if resource.sign_in_count == 1
-    return referrer_params[:user][:referrer] if useful_referrer?
+    return referrer_params[:user][:referrer] if useful_referrer? && valid_referrer?
     members_path
   end
 
@@ -10,6 +10,12 @@ class Users::SessionsController < Devise::SessionsController
   def useful_referrer?
     referrer_params[:user][:referrer].present? &&
       referrer_params[:user][:referrer] != '/login'
+  end
+  
+  def valid_referrer?
+  	referrer_params[:user][:referrer].in? %w[
+  	  /members /minutes
+  	]
   end
 
   def referrer_params
