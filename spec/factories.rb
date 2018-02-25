6e0712b4d1990600a1efebffa70dfa92b3fa5762
Julian Fiander <jsfiander@gmail.com>
2018-02-25 16:48:02 -0500

    Update factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,24 +1,48 @@
 FactoryBot.define do
   factory :header_image do
-    image_file ""
+    image_file ''
   end
+
   factory :photo do
     album_id 1
   end
+
   factory :album do
-    name "MyString"
+    name 'Test Album'
   end
+
   factory :role do
     sequence(:name) { |n| "This is example #{n}." }
   end
 
   factory :user do
-    sequence(:email) { |n| "example-#{n}@example.com" }
+    email "example-#{SecureRandom.hex(8)}@example.com"
     password SecureRandom.hex(16)
+  end
+
+  factory :event do
+    association :event_type
+    start_at Time.now + 1.week
+  end
+
+  factory :event_type do
+    event_category 'public'
+    title "America's Boating Course"
   end
 
   factory :user_role do
     association :user
     association :role
+  end
+
+  factory :bridge_office do
+    office 'commander'
+    association :user
+  end
+
+  factory :committee do
+    name 'rendezvous'
+    department 'administrative'
+    association :user
   end
 end
