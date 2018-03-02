780cd7e5c83fa5f82e9503f8506050d64ea152c2
Julian Fiander <jsfiander@gmail.com>
2018-03-01 21:14:33 -0500

    Attach PDF flyer when present

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -20,6 +20,7 @@ class RegistrationMailer < ApplicationMailer
     @signature = signature_for_confirm
     to = @registration&.user&.email || @registration.email
     from = "\"#{@signature[:name]}\" <#{@signature[:email]}>"
+    attach_pdf if attachable?
 
     mail(to: to, from: from, subject: 'Registration confirmation')
   end
@@ -30,23 +31,23 @@ class RegistrationMailer < ApplicationMailer
     if @registration.event.event_type.event_category == 'meeting'
       list = ['ao@bpsd9.org']
       list << if @registration.event.event_type.title == 'rendezvous'
-        get_chair_email('Rendezvous')
-      else
-        get_chair_email('Meetings & Programs')
-      end
+                get_chair_email('Rendezvous')
+              else
+                get_chair_email('Meetings & Programs')
+              end
     else
       list = ['seo@bpsd9.org', 'aseo@bpsd9.org']
       list << @registration.event.instructors.map(&:email)
       list << case @registration.event.event_type.event_category
-      when 'seminar'
-        get_chair_email('Seminars')
-      when 'public'
-        get_chair_email('ABC')
-      when 'advanced_grade'
-        get_chair_email('Advanced Grades')
-      when 'elective'
-        get_chair_email('Electives')
-      end
+              when 'seminar'
+                get_chair_email('Seminars')
+              when 'public'
+                get_chair_email('ABC')
+              when 'advanced_grade'
+                get_chair_email('Advanced Grades')
+              when 'elective'
+                get_chair_email('Electives')
+              end
     end
 
     list.flatten.uniq.reject(&:blank?)
@@ -85,5 +86,17 @@ class RegistrationMailer < ApplicationMailer
       name: BridgeOffice.find_by(office: 'educational').user.full_name,
       email: 'seo@bpsd9.org'
     }
+  end
+
+  def attach_pdf
+    flyer = @registration.event.flyer
+    data = Paperclip.io_adapters.for(flyer).read
+    name = @registration.event.event_type.display_title.delete("' ")
+    attachments["#{name}.pdf"] = data
+  end
+
+  def attachable?
+    @registration.event.flyer.present? &&
+      @registration.event.flyer.content_type == 'application/pdf'
   end
 end
