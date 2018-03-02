1823809a2d8a747527cc0290eaa6c1e8c2f439a1
Julian Fiander <jsfiander@gmail.com>
2018-03-02 12:53:19 -0500

    Move editor scripts and styles into head

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
   def editor(partial, options = {})
+    content_for :head do
+      <<~HTML.html_safe
+        <script>#{render('application/show_editor.js', page_name: partial)}</script>
+        <script>#{render('application/hide_editor.js', page_name: partial)}</script>
+        <noscript><style>div#editor{display:block;}a#show-editor,a#hide-editor{display:none;}</style></noscript>
+      HTML
+    end
+
     <<~HTML.html_safe
-      <script>#{render('application/show_editor.js', page_name: partial)}</script>
-      <script>#{render('application/hide_editor.js', page_name: partial)}</script>
-      <noscript><style>div#editor{display:block;}a#show-editor,a#hide-editor{display:none;}</style></noscript>
       <div id='editor-buttons'>
         <a href='#' id='show-editor' class='medium #{auto_show(partial)}'>Show Editor</a>
         <a href='#' id='hide-editor' class='medium #{auto_show(partial)}'>Hide Editor</a>
