1181cbef7f89097819b88376acf9afc195e9988a
Julian Fiander <jsfiander@gmail.com>
2018-03-03 16:11:49 -0500

    Clean up Users::SessionsController

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,22 +1,30 @@
 class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     return welcome_path if resource.sign_in_count == 1
-    return referrer_params[:user][:referrer] if useful_referrer? && valid_referrer?
+    return referrer_params[:user][:referrer] if useful_referrer?
     members_path
   end
 
   private
 
   def useful_referrer?
-    begin
-      referrer_params[:user][:referrer].present? &&
-        referrer_params[:user][:referrer] != '/login'
-    rescue
-      false
-    end
+    referrer_params[:user][:referrer].present? &&
+      referrer_params[:user][:referrer] != '/login' &&
+      valid_referrer?
+  rescue
+    false
   end
 
   def valid_referrer?
+    member_paths = referrer_paths.join('|')
+    referrer_params[:user][:referrer].match %r{\A/(#{member_paths})\z}
+  end
+
+  def referrer_params
+    params.permit(user: :referrer)
+  end
+
+  def referrer_paths
     # Whitelist for valid member routes
     #
     # This whitelist is permissions-agnostic, since Devise will handle that
@@ -29,18 +37,6 @@ class Users::SessionsController < Devise::SessionsController
     minutes_paths = '((minutes|excom)/\d{4}/\d{1,2})'
     event_paths = '((courses|seminars|events)/(new|(\d+(/(copy|edit))?)))'
     edit_paths = "edit/(#{MarkdownHelper::VIEWS.map { |_, v| v }.join('|')})"
-
-    member_paths = [
-      simple_paths.join('|'),
-      profile_paths,
-      minutes_paths,
-      event_paths,
-      edit_paths
-    ].join('|')
-    referrer_params[:user][:referrer].match %r{\A/(#{member_paths})\z}
-  end
-
-  def referrer_params
-    params.permit(user: :referrer)
+    [simple_paths, profile_paths, minutes_paths, event_paths, edit_paths]
   end
 end
