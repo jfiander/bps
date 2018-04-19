f08c8d294705ea6b18505c23ec9ef2dfa872dcd5
Julian Fiander <jsfiander@gmail.com>
2018-04-19 16:03:56 -0400

    Add approval mailers

--- app/mailers/member_application_mailer.rb
+++ app/mailers/member_application_mailer.rb
@@ -10,16 +10,22 @@ class MemberApplicationMailer < ApplicationMailer
   end
 
   def confirm(application)
-    @application = application
-    to = @application.primary.email
-    ao = BridgeOffice.includes(:user).find_by(office: 'administrative')
-    @signature = {
-      office: ao.office.titleize,
-      email: ao.email,
-      name: ao.user.full_name
-    }
+    prep_external(application)
 
-    mail(to: to, subject: 'Member application received!')
+    mail(to: @to, subject: 'Member application received!')
+  end
+
+  def approved(application)
+    prep_external(application)
+
+    mail(to: @to, subject: 'Member application approved!')
+  end
+
+  def approval_notice(application)
+    @application = application
+    @to_list = new_app_to
+
+    mail(to: @to_list, subject: 'Member application approved')
   end
 
   private
@@ -30,5 +36,16 @@ class MemberApplicationMailer < ApplicationMailer
       StandingCommitteeOffice.current.where(committee_name: 'executive')
                              .map(&:user).map(&:email)
     ].flatten.uniq
+  end
+
+  def prep_external(application)
+    @application = application
+    @to = @application.primary.email
+    ao = BridgeOffice.includes(:user).find_by(office: 'administrative')
+    @signature = {
+      office: ao.office.titleize,
+      email: ao.email,
+      name: ao.user.full_name
+    }
   end
 end
--- app/mailers/previews/member_application_mailer_preview.rb
+++ app/mailers/previews/member_application_mailer_preview.rb
@@ -8,9 +8,25 @@ class MemberApplicationPreview < ActionMailer::Preview
     MemberApplicationMailer.confirm(application)
   end
 
+  def approved
+    MemberApplicationMailer.approved(application)
+  end
+
+  def approved_three
+    MemberApplicationMailer.approved(large_application)
+  end
+
+  def approval_notice
+    MemberApplicationMailer.approval_notice(application)
+  end
+
   private
 
   def application
     MemberApplication.last
+  end
+
+  def large_application
+    MemberApplication.all.select { |m| m.member_applicants.count > 2 }.last
   end
 end
