f85443d06f0fc0e990d4930e98d861badbfb83dc
Julian Fiander <jsfiander@gmail.com>
2018-06-04 11:50:19 -0400

    Fix class declaration on CalendarHelper spec

--- spec/helpers/calendar_helper_spec.rb
+++ spec/helpers/calendar_helper_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe BucketHelper, type: :helper do
+RSpec.describe CalendarHelper, type: :helper do
   it 'should generate a correctly formatted calendar URL' do
     expect(render_calendar('12345:FFFFFF/67890:ABCDEF')).to eql(
       'https://calendar.google.com/calendar/b/2/embed?showTitle=0&showPrint=0' \
