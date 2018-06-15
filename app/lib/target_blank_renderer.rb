3f5a0eb0985f31d93262aa93cdb8322b400fd68d
Julian Fiander <jsfiander@gmail.com>
2018-06-15 12:43:44 -0400

    Clean up TargetBlankRenderer

--- app/lib/target_blank_renderer.rb
+++ app/lib/target_blank_renderer.rb
@@ -1,13 +1,13 @@
 class TargetBlankRenderer < Redcarpet::Render::HTML
   def link(link, title, alt_text)
-    if link.match(/^https?:\/\/#{ENV["DOMAIN"]}\/.*/) || link.match(/^\//)
-      "<a href='#{link}'>#{alt_text}</a>"
+    if link.match(%r{^https?://#{ENV["DOMAIN"]}/.*}) || link.match(%r{^/})
+      "<a href='#{link}' title='#{title}'>#{alt_text}</a>"
     else
-      "<a target='_blank' href='#{link}'>#{alt_text}</a>"
+      "<a target='_blank' href='#{link}' title='#{title}'>#{alt_text}</a>"
     end
   end
 
-  def autolink(link, link_type)
+  def autolink(link, _link_type)
     link(link, nil, link)
   end
 end
