ac6f99ad66efd0ea4348c2f4beb75f32abb03f0a
Julian Fiander <jsfiander@gmail.com>
2018-06-28 15:34:15 -0400

    Shorten label for registration number

--- app/lib/float_plan_pdf/boat_information.rb
+++ app/lib/float_plan_pdf/boat_information.rb
@@ -15,7 +15,7 @@ module FloatPlanPDF::BoatInformation
     )
     labeled_text('Color', "#{float_plan.hull_color} (hull)", x1: 300, x2: 360, y: 500)
     draw_text "#{float_plan.trim_color} (trim)", size: 12, at: [(420 + float_plan.hull_color.length * 4), 500] if float_plan.trim_color.present?
-    labeled_text('Registration', float_plan.registration_number, x1: 300, x2: 360, y: 480)
+    labeled_text('Reg num', float_plan.registration_number, x1: 300, x2: 360, y: 480)
     labeled_text('Engines', float_plan.engines, x1: 300, x2: 360, y: 460)
     labeled_text('Fuel cap', float_plan.fuel, x1: 300, x2: 360, y: 440)
   end
