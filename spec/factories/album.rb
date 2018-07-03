2a7440ac9dff6c49eecf599425df558f087438d6
Julian Fiander <jsfiander@gmail.com>
2018-07-03 14:33:12 -0400

    Rename factory files
    Includes some minor cleanup

---
+++ spec/factories/album.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+FactoryBot.define do
+  factory :album do
+    name 'Test Album'
+  end
+end
--- spec/factories/album_factory.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-FactoryBot.define do
-  factory :album do
-    name 'Test Album'
-  end
-end
-                                                                                                                                                                                                                        
