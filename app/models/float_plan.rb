abdfd7ba6d560bbe9087ad65fe881f6837b0fe35
Julian Fiander <jsfiander@gmail.com>
2018-07-03 14:36:01 -0400

    Clean up FloatPlan model
    Split formatting helpers into module
    Clean up onboard alias

--- app/models/float_plan.rb
+++ app/models/float_plan.rb
@@ -1,4 +1,6 @@
 class FloatPlan < ApplicationRecord
+  include Concerns::FloatPlan::Formatting
+
   has_many :float_plan_onboards
   accepts_nested_attributes_for :float_plan_onboards
 
@@ -13,9 +15,7 @@ class FloatPlan < ApplicationRecord
 
   validates_attachment_content_type(:pdf, content_type: %r{\Aapplication/pdf\z})
 
-  def onboard
-    float_plan_onboards
-  end
+  alias onboard :float_plan_onboards
 
   def generate_pdf
     update!(pdf: FloatPlanPDF.for(self))
@@ -23,67 +23,5 @@ class FloatPlan < ApplicationRecord
 
   def link
     FloatPlan.buckets[:floatplans].link("#{id}.pdf")
-  end
-
-  def fuel
-    if fuel_capacity.present?
-      "#{fuel_capacity} gallons"
-    else
-      'n/a'
-    end
-  end
-
-  def engines
-    if engines?
-      [
-        number_of_engines,
-        [engine_types, hp].reject(&:blank?).join(' – ')
-      ].join(' ')
-    else
-      'n/a'
-    end
-  end
-
-  def boat_type_display
-    "#{boat_type}#{subtype.present? ? " (#{subtype})" : ''}"
-  end
-
-  def radio_bands
-    [
-      flag(radio_vhf, 'VHF'),
-      flag(radio_ssb, 'SSB'),
-      flag(radio_cb, 'CB'),
-      flag(radio_cell_phone, 'Cell Phone')
-    ].compact.join(' / ')
-  end
-
-  def epirb_freqs
-    [
-      flag(epirb_16, 'Ch 15/16'),
-      flag(epirb_1215, '121.5 MHz'),
-      flag(epirb_406, '406 MHz')
-    ].compact.join(' / ')
-  end
-
-  private
-
-  def engines?
-    number_of_engines.present? ||
-      engine_type_1.present? ||
-      engine_type_2.present? ||
-      horse_power.present?
-  end
-
-  def engine_types
-    engine_types = [engine_type_1, engine_type_2].reject(&:blank?).join(' / ')
-    engine_types.present? ? "(#{engine_types})" : nil
-  end
-
-  def hp
-    horse_power.present? ? "#{horse_power} HP" : nil
-  end
-
-  def flag(bool, string)
-    bool ? string : nil
   end
 end
