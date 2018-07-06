ed9a8bb95dc7d6b7d396ddbd128ed134000b9094
Julian Fiander <jsfiander@gmail.com>
2018-07-06 17:26:22 -0400

    Fix HeaderImage aspect ratio error messages

--- app/models/header_image.rb
+++ app/models/header_image.rb
@@ -37,20 +37,20 @@ class HeaderImage < ApplicationRecord
 
   def add_errors(dimensions, ratio)
     errors.add(:file, too_small) if dimensions.width < 750
-    errors.add(:file, too_narrow) if ratio > 3.5
-    errors.add(:file, too_wide) if ratio < 2.75
+    errors.add(:file, too_wide) if ratio > 3.5
+    errors.add(:file, too_narrow) if ratio < 2.75
   end
 
   def too_small
     'must be at least 1000px wide'
   end
 
-  def too_narrow
-    'aspect ratio > 3.5:1 (make it wider)'
+  def too_wide
+    'aspect ratio > 3.5:1 (make it narrower)'
   end
 
-  def too_wide
-    'aspect ratio < 2.75:1 (make it narrower)'
+  def too_narrow
+    'aspect ratio < 2.75:1 (make it wider)'
   end
 
   def no_errors_yet?
