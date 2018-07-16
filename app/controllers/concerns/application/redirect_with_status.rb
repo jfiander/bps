6dc5d24b4b9f243b401f1948a38b57d1ad609483
Julian Fiander <jsfiander@gmail.com>
2018-07-16 13:29:38 -0400

    Add redirect_or_render_error method

--- app/controllers/concerns/application/redirect_with_status.rb
+++ app/controllers/concerns/application/redirect_with_status.rb
@@ -4,15 +4,29 @@ module Concerns::Application::RedirectWithStatus
   private
 
   def redirect_with_status(path, object:, verb:, past: nil, ivar: nil)
-    past ||= "#{verb}d"
-    if yield
-      redirect_to(path, success: "Successfully #{past} #{object}.")
-    else
+    redirect_if(yield, path, verb, past, object) do
       redirect_to(
         path,
         alert: "Unable to #{verb} #{object}.",
         error: errors(ivar, object)
       )
+    end
+  end
+
+  def redirect_or_render_error(path, render_method:, object:, verb:, past: nil, ivar: nil)
+    redirect_if(yield, path, verb, past, object) do
+      flash.now[:alert] = "Unable to #{verb} #{object}."
+      flash.now[:error] = errors(ivar, object)
+      render render_method
+    end
+  end
+
+  def redirect_if(condition, path, verb, past, object)
+    if condition
+      past ||= "#{verb}d"
+      redirect_to(path, success: "Successfully #{past} #{object}.")
+    else
+      yield
     end
   end
 
