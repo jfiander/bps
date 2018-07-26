d1ad58108141aacf49c8de4a686489e6315252df
Julian Fiander <jsfiander@gmail.com>
2018-07-26 13:12:29 -0400

    Improve login referrer handling
    Also adds certificate route

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,23 +3,18 @@
 class Users::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     return welcome_path if resource.sign_in_count == 1
-    return referrer_params[:user][:referrer] if useful_referrer?
+    return referrer_params[:user][:referrer] if valid_referrer?
     members_path
   end
 
   private
 
-  def useful_referrer?
-    referrer_params[:user][:referrer].present? &&
-      referrer_params[:user][:referrer] != '/login' &&
-      valid_referrer?
-  rescue
-    false
-  end
-
   def valid_referrer?
-    member_paths = referrer_paths.join('|')
-    referrer_params[:user][:referrer].match %r{\A/(#{member_paths})\z}
+    valid_pattern = %r{\A/?(?!login)(#{referrer_paths.join('|')})\z}
+    return false unless referrer_params[:user][:referrer].present?
+    referrer_params[:user][:referrer].match?(valid_pattern)
+  rescue StandardError
+    false
   end
 
   def referrer_params
@@ -32,14 +27,15 @@ class Users::SessionsController < Devise::SessionsController
     # This whitelist is permissions-agnostic, since Devise will handle that
     # after login is complete.
     simple_paths = %w[
-      members minutes users permit invitation/new header file import
+      members minutes profile permit invitation/new header file import
       user_help profile profile/edit ranks auto_permissions locations
       roster update_roster float_plan float_plans
     ]
-    profile_paths = '(users/(\d+|current))'
-    minutes_paths = '((minutes|excom)/\d{4}/\d{1,2})'
-    event_paths = '((courses|seminars|events)/(new|(\d+(/(copy|edit))?)))'
+    users_paths = 'users(/\d+(/certificate(.pdf)?)?)?'
+    minutes_paths = '(minutes|excom)/\d{4}/\d{1,2}'
+    event_paths = '(courses|seminars|events)(/(new|(\d+(/(copy|edit))?))?)'
     edit_paths = "edit/(#{MarkdownHelper::VIEWS.map { |_, v| v }.join('|')})"
-    [simple_paths, profile_paths, minutes_paths, event_paths, edit_paths]
+
+    [simple_paths, users_paths, minutes_paths, event_paths, edit_paths].flatten
   end
 end
