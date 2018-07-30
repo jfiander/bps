ad1ae942fe3a5080ccb896e56e69164d7b8d576a
Julian Fiander <jsfiander@gmail.com>
2018-07-30 14:23:54 -0400

    Bump version of fa_rails

--- spec/helpers/navigation_helper_spec.rb
+++ spec/helpers/navigation_helper_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe NavigationHelper, type: :helper do
       expect(link(:login_or_logout)).to eql(
         '<a class="red" title="Logout" rel="nofollow" data-method="delete" '\
         'href="/logout"><li class="">' \
-        "<i class='far fa-sign-out' data-fa-transform='' title=''></i>" \
+        "<i class='far fa-sign-out fa-1x' data-fa-transform='' title=''></i>" \
         'Logout</li></a>'
       )
     end
@@ -38,7 +38,7 @@ RSpec.describe NavigationHelper, type: :helper do
     it 'should generate the correct login link' do
       expect(link(:login_or_logout)).to eql(
         '<a class="members" title="Member Login" href="/login"><li class="">' \
-        "<i class='far fa-sign-in' data-fa-transform='' title=''></i>" \
+        "<i class='far fa-sign-in fa-1x' data-fa-transform='' title=''></i>" \
         'Member Login</li></a>'
       )
     end
