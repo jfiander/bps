713773d8ca99f9a6136f24a559e22fbd556d8ec8
Julian Fiander <jsfiander@gmail.com>
2018-08-09 19:24:18 -0400

    Simplify set_parents

--- app/services/import_users.rb
+++ app/services/import_users.rb
@@ -197,18 +197,22 @@ class ImportUsers
 
   def set_parents(parsed_csv)
     parsed_csv.each do |row|
-      next if row['Prim.Cert'].blank?
-      next unless (parent = User.find_by(certificate: row['Prim.Cert']))
-
+      next unless (parent = parent?(row))
       assign_parent(row, parent)
     end
   end
 
+  def parent?(row)
+    return if row['Prim.Cert'].blank?
+    User.find_by(certificate: row['Prim.Cert'])
+  end
+
   def assign_parent(row, parent)
     user = User.find_by(certificate: row['Certificate'])
+
     user&.update(parent_id: parent.id)
 
-    @families[user.parent_id] ||= []
-    @families[user.parent_id] << user.id
+    @families[user&.parent_id] ||= []
+    @families[user&.parent_id] << user&.id
   end
 end
