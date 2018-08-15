3542a6f3395d2a8e4e71ca9218886d1afd55f6ae
Julian Fiander <jsfiander@gmail.com>
2018-08-15 18:38:17 -0400

    Further clean up ImportUsers::CleanDate

--- app/services/import_users/clean_date.rb
+++ app/services/import_users/clean_date.rb
@@ -8,21 +8,31 @@ module ImportUsers
     end
 
     def call
-      clean_date unless @string.blank?
+      clean_date
     rescue ArgumentError => e
-      raise e unless e.message == 'invalid date'
+      handle_invalid_date(e)
     end
 
     private
 
     def clean_date
+      return if @string.blank?
       datestring = @string.ljust(5, '0').ljust(6, '1')
       datestring[datestring.length - 1] = '1' if datestring.last(2) == '00'
+      parse_date(datestring)
+    end
+
+    def parse_date(datestring)
       if datestring.length == 6
         Date.strptime(datestring, '%Y%m')
       elsif datestring.length == 8
         Date.strptime(datestring, '%Y%m%d')
       end
+    end
+
+    def handle_invalid_date(error)
+      raise error unless error.message == 'invalid date'
+      puts 'Invalid date' unless Rails.env.test?
     end
   end
 end
