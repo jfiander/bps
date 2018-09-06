4ceb9fc2d150190e395cc0b48b9ef808abf60949
Julian Fiander <jsfiander@gmail.com>
2018-09-05 20:30:56 -0400

    Add by argument to request_schedule mailer call

--- app/controllers/concerns/user/request_schedule.rb
+++ app/controllers/concerns/user/request_schedule.rb
@@ -4,7 +4,7 @@ module User::RequestSchedule
   def request_schedule
     @event = Event.find_by(id: clean_params[:id])
 
-    RegistrationMailer.request_schedule(@event.event_type).deliver
+    RegistrationMailer.request_schedule(@event.event_type, by: current_user).deliver
     flash[:success] = "#{@event.category.titleize} requested!"
   end
 end
