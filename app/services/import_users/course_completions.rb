dc5489e7a84fd9ed18365606121d66c2e6ab6a8e
Julian Fiander <jsfiander@gmail.com>
2018-09-12 12:51:55 -0400

    Clean up ImportUsers::CourseCompletions#call

--- app/services/import_users/course_completions.rb
+++ app/services/import_users/course_completions.rb
@@ -11,19 +11,21 @@ module ImportUsers
 
     def call
       course_completions_data.each do |(key, date)|
-        next if date.nil?
-        date = ImportUsers::CleanDate.new(date).call
-        next if course_completion_exists?(key, date)
+        next unless (date = ImportUsers::CleanDate.new(date).call)
 
-        @completions << CourseCompletion.create!(
-          user: @user, course_key: key, date: date
-        )
+        @completions << create_completion(key, date) unless exists?(key, date)
       end
 
       @completions
     end
 
     private
+
+    def create_completion(key, date)
+      CourseCompletion.create!(
+        user: @user, course_key: key, date: date
+      )
+    end
 
     def completion_ignored_columns
       ImportUsers::IMPORTED_FIELDS + ImportUsers::IGNORED_FIELDS
@@ -33,7 +35,7 @@ module ImportUsers
       @row.to_hash.except(*completion_ignored_columns)
     end
 
-    def course_completion_exists?(key, date)
+    def exists?(key, date)
       date.blank? ||
         CourseCompletion.find_by(user: @user, course_key: key).present?
     end
