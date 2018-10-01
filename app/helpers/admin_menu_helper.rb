3f7807bb84b1e16103a971ab1a7a805c93ea9f4c
Julian Fiander <jsfiander@gmail.com>
2018-10-01 15:05:14 -0400

    Fix Current Page admin menu section display
    Also clean up AdminMenuHelper

--- app/helpers/admin_menu_helper.rb
+++ app/helpers/admin_menu_helper.rb
@@ -12,82 +12,85 @@ module AdminMenuHelper
     end.reduce({}, :merge)
   end
 
+  def admin_current?
+    admin_markdown? || admin_events? || admin_otw? || admin_event_attachments?
+  end
+
   private
+
+  def admin_markdown?
+    current_user&.permitted?(:page) &&
+      controller.action_name.in?(StaticPage.names)
+  end
+
+  def admin_events?
+    current_user&.permitted?(event_type_param) &&
+      controller_name.in?(%w[courses seminars events])
+  end
+
+  def admin_otw?
+    current_user&.permitted?(:otw) &&
+      controller_name.in?(%w[otw_trainings])
+  end
+
+  def admin_event_attachments?
+    current_user&.permitted?(:event, :seminar, :course) &&
+      controller_name.in?(%w[event_types locations])
+  end
 
   def admin_current
     return unless admin_current?
-
     render('application/navigation/admin/current')
-  end
-
-  def admin_current?
-    (current_user&.permitted?(:page) &&
-      controller.action_name.in?(StaticPage.names)) ||
-      current_user&.permitted?(event_type_param, :education) ||
-      (
-        current_user&.permitted?(:event, :seminar, :course) &&
-        controller_name.in?(%w[event_types locations])
-      )
   end
 
   def admin_files
     return unless current_user&.permitted?(:page)
-
     render('application/navigation/admin/files')
   end
 
   def admin_users_top
     return unless current_user&.permitted?(:users)
-
     render('application/navigation/admin/users_top')
   end
 
   def admin_review
-    return unless
-      current_user&.permitted?(:users, :float, :roster) || current_user&.excom?
-
+    roles = %i[users float roster]
+    return unless current_user&.permitted?(roles) || current_user&.excom?
     render('application/navigation/admin/review')
   end
 
   def admin_upload
     return unless current_user&.permitted?(:users, :roster)
-
     render('application/navigation/admin/upload')
   end
 
   def admin_roster
     return unless current_user&.permitted?(:users, :roster)
-
     render('application/navigation/admin/roster')
   end
 
   def admin_users_bottom
     return unless current_user&.permitted?(:users)
-
     render('application/navigation/admin/users_bottom')
   end
 
   def admin_education
     return unless current_user&.permitted?(:education)
-
     render('application/navigation/admin/education')
   end
 
   def admin_otw
     return unless current_user&.permitted?(:education)
-
     render('application/navigation/admin/otw')
   end
 
   def admin_completions
     return unless current_user&.permitted?(:education)
-
     render('application/navigation/admin/completions')
   end
 
   def admin_admin
     return unless current_user&.permitted?(:admin, strict: true)
-
     render('application/navigation/admin/admin')
   end
 end
