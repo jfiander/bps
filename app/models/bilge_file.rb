baaa4938bc0415844ba1b1d9b8cbcf3e73f944b4
Julian Fiander <jsfiander@gmail.com>
2018-10-02 14:49:23 -0400

    Add automatic notification on new Bilge upload

--- app/models/bilge_file.rb
+++ app/models/bilge_file.rb
@@ -11,6 +11,8 @@ class BilgeFile < UploadedFile
   scope :ordered, -> { order(year: :asc, month: :asc) }
   scope :last_18, -> { order(year: :desc, month: :desc).limit(18) }
 
+  after_create { send_notification }
+
   def self.issues
     {
       1 => 'Jan', 2 => 'Feb', 3 => 'Mar', 4 => 'Apr', 5 => 'May', 6 => 'Jun',
@@ -28,5 +30,18 @@ class BilgeFile < UploadedFile
 
   def link
     self.class.buckets[:bilge].link(file.s3_object.key)
+  end
+
+  private
+
+  def send_notification
+    SlackNotification.new(
+      type: :info, title: 'Bilge Chatter Uploaded',
+      fallback: 'A new issue of The Bilge Chatter has been uploaded.',
+      fields: {
+        'Issue' => full_issue,
+        'Link' => "<#{link}|Bilge Chatter>"
+      }
+    ).notify!
   end
 end
