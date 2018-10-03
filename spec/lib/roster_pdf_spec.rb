e18e07d9053306c52ff2811c4b09e2d7951d1c7c
Julian Fiander <jsfiander@gmail.com>
2018-10-03 19:35:15 -0400

    Add spec to cover birthday format

--- spec/lib/roster_pdf_spec.rb
+++ spec/lib/roster_pdf_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe RosterPDF, type: :lib do
       FactoryBot.create(:user, life: '2017-03-05', grade: 'SN', ed_ach: '2018-01-01', mm: 35)
       FactoryBot.create(:user, life: '2001-03-05', grade: 'SN', ed_ach: '2002-01-01', mm: 50)
       FactoryBot.create(:user, grade: 'N', ed_pro: '2017-01-01', senior: '2013-01-01', mm: 5)
-      FactoryBot.create(:user, grade: 'JN', ed_pro: '2016-01-01')
+      FactoryBot.create(:user, grade: 'JN', ed_pro: '2016-01-01', birthday: '2001-06-05')
       u = FactoryBot.create(:user, city: 'Someplace', state: 'ST')
       FactoryBot.create(:bridge_office, office: :commander, user: u)
       FactoryBot.create(:roster_award_recipient, award_name: 'Bill Booth Moose Milk', user: u, year: Date.today.strftime('%Y-%m-%d'), photo: File.new(test_image(300, 500)))
