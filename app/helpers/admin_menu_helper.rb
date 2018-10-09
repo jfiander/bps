7904240be2f4523c17697b63429b756f781a697e
Julian Fiander <jsfiander@gmail.com>
2018-10-09 12:34:36 -0400

    Restructure AdminMenuHelper

--- app/helpers/admin_menu_helper.rb
+++ app/helpers/admin_menu_helper.rb
@@ -2,95 +2,81 @@
 
 module AdminMenuHelper
   def admin_menu
-    menus = %i[
-      current files users_top review upload roster users_bottom education otw
-      completions admin
-    ]
+    @admin_menu ||= admin_menus.map do |menu, permit|
+      next if permit == false
+      next unless permit == true || show_link?(*permit)
 
-    @admin_menu ||= menus.map do |menu|
-      { menu => send("admin_#{menu}") }
-    end.reduce({}, :merge)
+      { menu => render("application/navigation/admin/#{menu}") }
+    end.compact.reduce({}, :merge)
   end
 
   def admin_current?
     admin_markdown? || admin_events? || admin_otw? || admin_event_attachments?
   end
 
+  def show_link?(*roles, strict: false, **options)
+    req_cont = *options[:controller] || []
+    req_action = *options[:action] || []
+    not_cont = *options[:not_controller] || []
+    not_action = *options[:not_action] || []
+
+    return false unless current_user&.permitted?(roles, strict: strict)
+    return false if invalid?(req_cont, req_action, not_cont, not_action)
+    true
+  end
+
   private
 
+  def admin_menus
+    %i[
+      current files users_top review upload roster users_bottom education otw
+      completions admin
+    ]
+  end
+
+  def invalid?(rc, ra, nc, na)
+    return true if missing_controller?(rc) && ra.blank?
+    return true if wrong_controller?(nc) && na.blank?
+    return true if missing_action?(ra) && rc.blank?
+    return true if wrong_action?(na) && nc.blank?
+    return true if missing_controller?(rc) && missing_action?(ra)
+    return true if wrong_controller?(nc) && wrong_action?(na)
+    false
+  end
+
+  def missing_controller?(req_controller = nil)
+    return false unless req_controller.present?
+    !controller_name.in?(req_controller)
+  end
+
+  def wrong_controller?(not_controller = nil)
+    return false unless not_controller.present?
+    controller_name.in?(not_controller)
+  end
+
+  def missing_action?(req_action = nil)
+    return false unless req_action.present?
+    !controller.action_name.in?(req_action)
+  end
+
+  def wrong_action?(not_action = nil)
+    return false unless not_action.present?
+    controller.action_name.in?(not_action)
+  end
+
   def admin_markdown?
-    current_user&.permitted?(:page) &&
-      controller.action_name.in?(StaticPage.names)
+    show_link?(:page, action: StaticPage.names)
   end
 
   def admin_events?
-    current_user&.permitted?(event_type_param) &&
-      controller_name.in?(%w[courses seminars events])
+    show_link?(event_type_param, controller: %w[courses seminars events])
   end
 
   def admin_otw?
-    current_user&.permitted?(:otw) &&
-      controller_name.in?(%w[otw_trainings])
+    show_link?(:otw, controller: %w[otw_trainings])
   end
 
   def admin_event_attachments?
-    current_user&.permitted?(:event, :seminar, :course) &&
-      controller_name.in?(%w[event_types locations])
-  end
-
-  def admin_current
-    return unless admin_current?
-    render('application/navigation/admin/current')
-  end
-
-  def admin_files
-    return unless current_user&.permitted?(:page)
-    render('application/navigation/admin/files')
-  end
-
-  def admin_users_top
-    return unless current_user&.permitted?(:users)
-    render('application/navigation/admin/users_top')
-  end
-
-  def admin_review
-    roles = %i[users float roster excom]
-    return unless current_user&.permitted?(roles)
-    render('application/navigation/admin/review')
-  end
-
-  def admin_upload
-    return unless current_user&.permitted?(:users, :roster)
-    render('application/navigation/admin/upload')
-  end
-
-  def admin_roster
-    return unless current_user&.permitted?(:users, :roster)
-    render('application/navigation/admin/roster')
-  end
-
-  def admin_users_bottom
-    return unless current_user&.permitted?(:users)
-    render('application/navigation/admin/users_bottom')
-  end
-
-  def admin_education
-    return unless current_user&.permitted?(:education)
-    render('application/navigation/admin/education')
-  end
-
-  def admin_otw
-    return unless current_user&.permitted?(:education)
-    render('application/navigation/admin/otw')
-  end
-
-  def admin_completions
-    return unless current_user&.permitted?(:education)
-    render('application/navigation/admin/completions')
-  end
-
-  def admin_admin
-    return unless current_user&.permitted?(:admin, strict: true)
-    render('application/navigation/admin/admin')
+    show_link?(%i[event seminar course], controller: %w[event_types locations])
   end
 end
