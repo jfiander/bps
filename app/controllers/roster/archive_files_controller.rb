ffce9d4a442b681570f2d198659e2083d261627c
Julian Fiander <jsfiander@gmail.com>
2018-10-16 10:24:25 -0400

    Disable roster archive if most recent is current

--- app/controllers/roster/archive_files_controller.rb
+++ app/controllers/roster/archive_files_controller.rb
@@ -2,7 +2,10 @@
 
 class Roster::ArchiveFilesController < RosterController
   def create
-    if Roster::ArchiveFile.archive!
+    status = Roster::ArchiveFile.archive!
+    if status == :current
+      flash[:notice] = 'Latest archive is current.'
+    elsif status
       flash[:success] = 'Successfully archived current roster.'
     else
       flash[:alert] = 'Unable to archive current roster.'
--- app/models/roster/archive_file.rb
+++ app/models/roster/archive_file.rb
@@ -4,22 +4,28 @@ class Roster::ArchiveFile < UploadedFile
   end
 
   def self.archive!
-    generated_at = download_current_roster
-    create_new_archive(generated_at)
+    return :current if roster_generated_at == last&.generated_at
+    download_current_roster
+    create_new_archive(roster_generated_at)
   end
 
   def self.download_current_roster
-    year = Date.today.strftime('%Y')
-    file_name = "Birmingham_Power_Squadron_-_#{year}_Roster.pdf"
-    file = buckets[:files].download("roster/#{file_name}")
+    file = buckets[:files].download("roster/#{roster_file_name}")
     File.open("#{Rails.root}/tmp/run/roster.pdf", 'wb') { |f| f.write(file) }
-
-    buckets[:files].object("roster/#{file_name}").last_modified
   end
 
   def self.create_new_archive(generated_at)
     file = File.open("#{Rails.root}/tmp/run/roster.pdf", 'rb')
     Roster::ArchiveFile.create(file: file, generated_at: generated_at)
+  end
+
+  def self.roster_file_name
+    year = Date.today.strftime('%Y')
+    "Birmingham_Power_Squadron_-_#{year}_Roster.pdf"
+  end
+
+  def self.roster_generated_at
+    buckets[:files].object("roster/#{roster_file_name}").last_modified
   end
 
   def link
