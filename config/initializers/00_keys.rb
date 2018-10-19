0a9bf769f55a3c1337655f6da13c42df35cca3f9
Julian Fiander <julian@fiander.one>
2018-10-19 01:57:39 -0400

    Replace old CF key store with encrypted key

--- config/initializers/00_keys.rb
+++ config/initializers/00_keys.rb
@@ -10,11 +10,43 @@ def self.store_key(path, key = nil, overwrite: true)
   end
 end
 
-store_key(
-  Rails.application.secrets[:cf_private_key_path],
-  ENV['CLOUDFRONT_PRIVATE_KEY'],
-  overwrite: false
-)
+def decrypt_cf_key
+  cf_key_enc = File.join(Rails.root, 'config', 'keys', 'cf.pem.enc')
+
+  cipher = OpenSSL::Cipher.new('aes-256-cbc')
+  cipher.decrypt
+  cipher.key = Base64.decode64(ENV['CF_ENC_KEY'])
+  cipher.iv = Base64.decode64(ENV['CF_ENC_IV'])
+
+  buffer = +''
+  File.open("#{Rails.root}/config/keys/cf.pem", 'wb') do |outfile|
+    File.open(cf_key_enc, 'rb') do |infile|
+      outfile << cipher.update(buffer) while infile.read(4096, buffer)
+      outfile << cipher.final
+    end
+  end
+end
+
+# def encrypt_cf_key
+#   cf_key = File.join(Rails.root, 'config', 'keys', 'cf.pem')
+
+#   cipher = OpenSSL::Cipher.new('aes-256-cbc')
+#   cipher.encrypt
+#   key = Base64.encode64(cipher.random_key)
+#   iv = Base64.encode64(cipher.random_iv)
+
+#   buffer = +''
+#   File.open(File.join(Rails.root, 'config', 'keys', 'cf.pem.enc'), 'wb') do |outfile|
+#     File.open(File.join(Rails.root, 'config', 'keys', 'cf.pem'), 'rb') do |infile|
+#       outfile << cipher.update(buffer) while infile.read(4096, buffer)
+#       outfile << cipher.final
+#     end
+#   end
+
+#   { key: key, iv: iv }
+# end
+
+decrypt_cf_key
 
 store_key('config/keys/google_calendar_token.yaml') do |f|
   f.write("---\ndefault: '")
