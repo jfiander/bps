443af5698518ba47d8139b4d5352d95f1f83f2d5
Julian Fiander <jsfiander@gmail.com>
2019-01-14 17:06:18 -0500

    Fix DateHelper logic

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -3,21 +3,23 @@
 module DateHelper
   def next_excom(date = Date.today)
     month = excom_in_session?(date) ? next_excom_month(date) : 'September'
-    excom_date(month)
+    excom_date(month, date.strftime('%Y'))
   end
 
   def excom_in_session?(date = Date.today)
-    !date.between?(excom_date('June'), excom_date('September'))
+    !date.between?(
+      excom_date('June', date.strftime('%Y')), excom_date('September', date.strftime('%Y'))
+    )
   end
 
-  def excom_date(month = Date.today.strftime('%B'))
-    date = Date.strptime("#{Date.today.year} #{month}", '%Y %B')
+  def excom_date(month = Date.today.strftime('%B'), year = Date.today.strftime('%Y'))
+    date = Date.strptime("#{year} #{month}", '%Y %B')
     date += 1.day until date.strftime('%A') == 'Tuesday'
     date
   end
 
   def next_excom_month(date = Date.today)
-    date += 1.month if date > excom_date(date.strftime('%B'))
+    date += 1.month if date > excom_date(date.strftime('%B'), date.strftime('%Y'))
     date.strftime('%B')
   end
 
