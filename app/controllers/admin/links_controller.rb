bdca7e10d1eeca18881965977a206587a9297cee
Julian Fiander <jsfiander@gmail.com>
2019-02-08 00:01:22 -0500

    Add Sextant lease S3 links

--- app/controllers/admin/links_controller.rb
+++ app/controllers/admin/links_controller.rb
@@ -23,7 +23,9 @@ private
   def links
     {
       'abc' => seo_link('Courses/ABC3-16-hour.zip'),
-      'abc-8hr' => seo_link('Courses/ABC3-8-hour.zip')
+      'abc-8hr' => seo_link('Courses/ABC3-8-hour.zip'),
+      'Sextant Lease (fillable)' => seo_link('Sextant/fillable/SEXTANT_LEASE_AGREEMENT.pdf'),
+      'Sextant Lease (flat)' => seo_link('Sextant/SEXTANT_LEASE_AGREEMENT.pdf')
     }
   end
 
