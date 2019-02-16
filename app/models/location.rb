57e474dd8032552e99af8eda7ba85fc4998e3f6c
Julian Fiander <jsfiander@gmail.com>
2019-02-15 20:01:19 -0500

    Wrap static SQL in Arel

--- app/models/location.rb
+++ app/models/location.rb
@@ -20,7 +20,7 @@ class Location < ApplicationRecord
 
   validate :valid_map_link?
 
-  default_scope { order('favorite IS NULL, favorite DESC') }
+  default_scope { order(Arel.sql('favorite IS NULL, favorite DESC')) }
 
   def display
     return { id: 0, address: 'TBD' } unless address.present?
