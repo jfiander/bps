5200f85c3f50b0659bb5a16e536e4486e8c3c94e
Julian Fiander <jsfiander@gmail.com>
2019-04-11 17:26:46 -0400

    Move token storage into API modules

--- app/lib/google_api/base.rb
+++ app/lib/google_api/base.rb
@@ -2,22 +2,10 @@
 
 module GoogleAPI
   class Base
-    OOB_URI ||= 'urn:ietf:wg:oauth:2.0:oob'
-    AUTH_SCOPES ||= [
-      Google::Apis::AdminDirectoryV1::AUTH_ADMIN_DIRECTORY_GROUP,
-      Google::Apis::CalendarV3::AUTH_CALENDAR
-    ].freeze
+    include GoogleAPI::Concerns::Authorization
 
     def initialize(auth: false)
       self.authorize! if auth
-    end
-
-    def authorize!(refresh: false, reveal: false)
-      auth = authorize(refresh: refresh)
-      service.authorization = auth
-      return true unless reveal
-
-      [auth, auth_keys(auth)]
     end
 
   private
@@ -26,46 +14,6 @@ module GoogleAPI
       raise 'No service class defined.' unless defined?(service_class)
 
       @service ||= service_class.new
-    end
-
-    def authorize(user_id = 'default', refresh: false)
-      @credentials = authorizer.get_credentials(user_id)
-      return @credentials unless @credentials.nil? || refresh
-
-      @credentials = authorizer.get_and_store_credentials_from_code(
-        user_id: user_id, code: request_authorization, base_url: OOB_URI
-      )
-    end
-
-    def request_authorization
-      url = authorizer.get_authorization_url(base_url: OOB_URI)
-      puts("Open this URL to authorize:\n", url)
-      print("\nResponse code: ")
-      gets
-    end
-
-    def authorizer
-      @authorizer ||= Google::Auth::UserAuthorizer.new(auth_client_id, AUTH_SCOPES, auth_token_store)
-    end
-
-    def auth_client_id
-      Google::Auth::ClientId.from_hash(JSON.parse(File.read('config/keys/google_api_client.json')))
-    end
-
-    def auth_token_store
-      Google::Auth::Stores::FileTokenStore.new(file: 'config/keys/google_token.yaml')
-    end
-
-    def auth_keys(auth)
-      {
-        GOOGLE_CLIENT_ID: auth.client_id, GOOGLE_CLIENT_SECRET: auth.client_secret,
-        GOOGLE_ACCESS_TOKEN: auth.access_token, GOOGLE_REFRESH_TOKEN: auth.refresh_token,
-        GOOGLE_AUTH_SCOPE: auth.scope, GOOGLE_AUTH_EXP: expires_milli(auth.as_json['expires_at'])
-      }
-    end
-
-    def expires_milli(time)
-      DateTime.strptime(time, '%Y-%m-%dT%H:%M:%S.%L%:z').to_i * 1000
     end
   end
 end
--- config/initializers/00_keys.rb
+++ config/initializers/00_keys.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-# Creates key files from environment variables
-def self.store_key(path, key = nil, overwrite: true)
-  return if File.exist?(path) && !overwrite
+# # Creates key files from environment variables
+# def self.store_key(path, key = nil, overwrite: true)
+#   return if File.exist?(path) && !overwrite
 
-  File.open(path, 'w+') do |f|
-    File.chmod(0600, f)
-    block_given? ? yield(f) : f.write(key)
-  end
-end
+#   File.open(path, 'w+') do |f|
+#     File.chmod(0600, f)
+#     block_given? ? yield(f) : f.write(key)
+#   end
+# end
 
 def decrypt_cf_key
   cf_key_enc = File.join(Rails.root, 'config', 'keys', 'cf.pem.enc')
@@ -47,35 +47,3 @@ end
 # end
 
 decrypt_cf_key
-
-store_key('config/keys/google_api_client.json') do |f|
-  f.write('{"installed":{"client_id":"')
-  f.write(ENV['GOOGLE_CLIENT_ID'])
-  f.write('","project_id":"charming-scarab-208718",')
-  f.write('"auth_uri":"https://accounts.google.com/o/oauth2/auth",')
-  f.write('"token_uri":"https://accounts.google.com/o/oauth2/token",')
-  f.write('"auth_provider_x509_cert_url":')
-  f.write('"https://www.googleapis.com/oauth2/v1/certs",')
-  f.write('"client_secret":"')
-  f.write(ENV['GOOGLE_CLIENT_SECRET'])
-  f.write('","redirect_uris":["urn:ietf:wg:oauth:2.0:oob",')
-  f.write('"http://localhost"]}}')
-end
-
-store_key('config/keys/google_token.yaml') do |f|
-  f.write("---\n")
-  f.write("default: '")
-  f.write('{"client_id":"')
-  f.write(ENV['GOOGLE_CLIENT_ID'])
-  f.write('","access_token":"')
-  f.write(ENV['GOOGLE_ACCESS_TOKEN'])
-  f.write('","refresh_token":"')
-  f.write(ENV['GOOGLE_REFRESH_TOKEN'])
-  f.write('","scope":[')
-  f.write(ENV.select { |k, v| k =~ /GOOGLE_AUTH_SCOPE_/ }.values.map { |s| "\"#{s}\"" }.join(','))
-  f.write('],')
-  f.write('"expiration_time_millis":')
-  f.write(ENV['GOOGLE_AUTH_EXP'])
-  f.write('}')
-  f.write("'\n")
-end
