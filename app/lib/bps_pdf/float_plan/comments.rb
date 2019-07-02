284ab1f979226f1c70a918895c53dd4e8f4b54e5
Julian Fiander <jsfiander@gmail.com>
2019-07-02 16:23:19 -0400

    Remove boundary box line from float plan comments

--- app/lib/bps_pdf/float_plan/comments.rb
+++ app/lib/bps_pdf/float_plan/comments.rb
@@ -7,7 +7,6 @@ module BpsPdf
         width = @safety_y < 240 && float_plan.onboard.count <= 2 ? 250 : 500
         draw_text 'Comments:', size: 14, at: [25, (80 + @left_vertical_offset)]
         bounding_box([25, (60 + @left_vertical_offset)], width: width, height: comments_height) do
-          stroke_bounds
           text float_plan.comments, size: 12
         end
       end
