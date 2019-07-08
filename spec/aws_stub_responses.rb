f10aab5e3a222ee21bf3ef39206e28565544f17c
Julian Fiander <jsfiander@gmail.com>
2019-07-08 12:23:49 -0400

    Add Bilge distro stub

--- spec/aws_stub_responses.rb
+++ spec/aws_stub_responses.rb
@@ -10,6 +10,25 @@ Aws.config[:s3] = {
   }
 }
 
+def distro(key)
+  {
+    id: "test_#{key}_distro_id", status: 'Deployed',
+    arn: "arn:aws:cloudfront::test_distro:distribution:test_#{key}_distro_id",
+    domain_name: 'something.cloudfront.net', last_modified_time: Time.zone.now,
+    aliases: { items: ["#{key}.development.bpsd9.org"], quantity: 1 },
+    origins: { items: [{ id: 'stub', domain_name: "stub_#{key}.bpsd9.org" }], quantity: 1 },
+    default_cache_behavior: {
+      target_origin_id: 'stub', viewer_protocol_policy: 'stub', min_ttl: 1,
+      forwarded_values: { query_string: false, cookies: { forward: 'stub' } },
+      trusted_signers: { enabled: true, quantity: 1, items: ['stub'] }
+    },
+    cache_behaviors: { quantity: 0 }, custom_error_responses: { quantity: 0 },
+    restrictions: { geo_restriction: { restriction_type: 'none', quantity: 0 } },
+    comment: 'stub', price_class: 'Standard', enabled: true,
+    viewer_certificate: {}, web_acl_id: 'stub', http_version: '1.2', is_ipv6_enabled: false
+  }
+end
+
 Aws.config[:cloudfront] = {
   stub_responses: {
     create_invalidation: {
@@ -24,24 +43,10 @@ Aws.config[:cloudfront] = {
     list_distributions: {
       distribution_list: {
         items: [
-          {
-            id: 'test_distro_id', status: 'Deployed',
-            arn: 'arn:aws:cloudfront::test_distro:distribution:test_distro_id',
-            domain_name: 'something.cloudfront.net', last_modified_time: Time.zone.now,
-            aliases: { items: ['files.development.bpsd9.org'], quantity: 1 },
-            origins: { items: [{ id: 'stub', domain_name: 'stub.bpsd9.org' }], quantity: 1 },
-            default_cache_behavior: {
-              target_origin_id: 'stub', viewer_protocol_policy: 'stub', min_ttl: 1,
-              forwarded_values: { query_string: false, cookies: { forward: 'stub' } },
-              trusted_signers: { enabled: true, quantity: 1, items: ['stub'] }
-            },
-            cache_behaviors: { quantity: 0 }, custom_error_responses: { quantity: 0 },
-            restrictions: { geo_restriction: { restriction_type: 'none', quantity: 0 } },
-            comment: 'stub', price_class: 'Standard', enabled: true,
-            viewer_certificate: {}, web_acl_id: 'stub', http_version: '1.2', is_ipv6_enabled: false
-          }
+          distro(:files),
+          distro(:bilge)
         ],
-        marker: '12345', max_items: 1, is_truncated: false, quantity: 1
+        marker: '12345', max_items: 2, is_truncated: false, quantity: 2
       }
     }
   }
