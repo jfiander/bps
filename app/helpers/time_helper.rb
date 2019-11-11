82b550d2d053971da570b159267c44e62f6f19d8
Julian Fiander <jsfiander@gmail.com>
2019-11-11 15:44:26 -0500

    Add example outputs to TimeHelper

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module TimeHelper
-  FULL_TIME_FORMAT = '%A %d %B %Y at %H%M %Z'
-  LONG_TIME_FORMAT = '%a %d %b %Y @ %H%M %Z'
-  MEDIUM_TIME_FORMAT = '%-m/%-d/%Y @ %H%M'
-  SHORT_TIME_FORMAT = '%-m/%-d @ %H%M'
-  EVENT_DATE_FORMAT = '%a %d %b %Y'
-  SIMPLE_DATE_FORMAT = '%-m/%-d/%Y'
-  PUBLIC_DATE_FORMAT = '%b %-d'
-  PUBLIC_TIME_FORMAT = '%l:%M %P %Z'
-  DURATION_FORMAT = '%-kh %Mm'
-  ISO_TIME_FORMAT = '%Y-%m-%dT%H:%M'
+  FULL_TIME_FORMAT = '%A %d %B %Y at %H%M %Z' # Tuesday 01 January 2019 at 0000 EST
+  LONG_TIME_FORMAT = '%a %d %b %Y @ %H%M %Z'  # Tue 01 Jan 2019 @ 0000 EST
+  MEDIUM_TIME_FORMAT = '%-m/%-d/%Y @ %H%M'    # 1/1/2019 @ 0000
+  SHORT_TIME_FORMAT = '%-m/%-d @ %H%M'        # 1/1 @ 0000
+  EVENT_DATE_FORMAT = '%a %d %b %Y'           # Tue 01 Jan 2019
+  SIMPLE_DATE_FORMAT = '%-m/%-d/%Y'           # 1/1/2019
+  PUBLIC_DATE_FORMAT = '%b %-d'               # Jan 1
+  PUBLIC_TIME_FORMAT = '%l:%M %P %Z'          # 12:00 am EST
+  DURATION_FORMAT = '%-kh %Mm'                # 0h 00m
+  ISO_TIME_FORMAT = '%Y-%m-%dT%H:%M'          # 2019-01-01T00:00
 end
