67de1ef02e107e2f1c6097074dd896c57b4abb3c
Julian Fiander <jsfiander@gmail.com>
2019-12-11 20:49:25 -0500

    Make profile photo filename check case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,7 +60,7 @@ class User < ApplicationRecord
 
   validate :valid_rank, :valid_grade
   validates_attachment_content_type :profile_photo, content_type: %r{\Aimage/}
-  validates_attachment_file_name :profile_photo, matches: /(\.png|\.jpe?g)\z/
+  validates_attachment_file_name :profile_photo, matches: /(\.png|\.jpe?g)\z/i
   validates :certificate, uniqueness: true, allow_nil: true
 
   scope :locked, -> { where.not(locked_at: nil) }
