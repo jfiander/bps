4f701df2c69df7b4e853d985574816b9f0c66a25
Julian Fiander <jsfiander@gmail.com>
2019-12-16 19:12:35 -0500

    Clean up FlagsHelper for rubocop

--- app/helpers/flags_helper.rb
+++ app/helpers/flags_helper.rb
@@ -5,9 +5,9 @@ module FlagsHelper
     content_tag(:div, class: 'officer-flags') do
       concat image_tag(@bucket.link("flags/PNG/#{rank}.thumb.png"), width: 150, height: height)
       concat tag(:br)
-      concat link_to('PNG', @bucket.link("flags/PNG/#{rank}.png"), disposition: 'inline')
+      concat dl_link('PNG', "flags/PNG/#{rank}.png")
       concat content_tag(:span, ' | ')
-      concat link_to('SVG', @bucket.link("flags/SVG/#{rank}.svg"), disposition: 'inline')
+      concat dl_link('SVG', "flags/SVG/#{rank}.svg")
     end
   end
 
@@ -15,9 +15,9 @@ module FlagsHelper
     content_tag(:div, class: 'officer-insignia') do
       concat image_tag(@bucket.link("flags/PNG/insignia/#{rank}.thumb.png"), height: 75)
       concat tag(:br)
-      concat link_to('PNG', @bucket.link("flags/PNG/insignia/#{rank}.png"), disposition: 'inline')
+      concat dl_link('PNG', "flags/PNG/insignia/#{rank}.png")
       concat content_tag(:span, ' | ')
-      concat link_to('SVG', @bucket.link("flags/SVG/insignia/#{rank}.svg"), disposition: 'inline')
+      concat dl_link('SVG', "flags/SVG/insignia/#{rank}.svg")
     end
   end
 
@@ -31,13 +31,13 @@ module FlagsHelper
       concat image_tag(@bucket.link("insignia/PNG/grades/tr/#{grade}#{edpro_tag}.png"), height: height)
       concat tag(:br)
       concat content_tag(:span, 'PNG: ')
-      concat link_to('T', @bucket.link("insignia/PNG/grades/tr/#{grade}#{edpro_tag}.png"), disposition: 'inline', title: 'Transparent background')
+      concat dl_link('T', "insignia/PNG/grades/tr/#{grade}#{edpro_tag}.png", 'Transparent background')
       concat content_tag(:span, ' ')
-      concat link_to('B', @bucket.link("insignia/PNG/grades/black/#{grade}#{edpro_tag}.png"), disposition: 'inline', title: 'Black background')
+      concat dl_link('B', "insignia/PNG/grades/black/#{grade}#{edpro_tag}.png", 'Black background')
       concat content_tag(:span, ' ')
-      concat link_to('W', @bucket.link("insignia/PNG/grades/white/#{grade}#{edpro_tag}.png"), disposition: 'inline', title: 'White background')
+      concat dl_link('W', "insignia/PNG/grades/white/#{grade}#{edpro_tag}.png", 'White background')
       concat content_tag(:span, ' | ')
-      concat link_to('SVG', @bucket.link("insignia/SVG/grades/#{grade}#{edpro_tag}.svg"), disposition: 'inline')
+      concat dl_link('SVG', "insignia/SVG/grades/#{grade}#{edpro_tag}.svg")
     end
   end
 
@@ -46,23 +46,29 @@ module FlagsHelper
       concat image_tag(@bucket.link("insignia/PNG/membership/tr/#{membership}.png"), width: 250)
       concat tag(:br)
       concat content_tag(:span, 'PNG: ')
-      concat link_to('T', @bucket.link("insignia/PNG/membership/tr/#{membership}.png"), disposition: 'inline', title: 'Transparent background')
+      concat dl_link('T', "insignia/PNG/membership/tr/#{membership}.png", 'Transparent background')
       concat content_tag(:span, ' ')
-      concat link_to('B', @bucket.link("insignia/PNG/membership/black/#{membership}.png"), disposition: 'inline', title: 'Black background')
+      concat dl_link('B', "insignia/PNG/membership/black/#{membership}.png", 'Black background')
       concat content_tag(:span, ' ')
-      concat link_to('W', @bucket.link("insignia/PNG/membership/white/#{membership}.png"), disposition: 'inline', title: 'White background')
+      concat dl_link('W', "insignia/PNG/membership/white/#{membership}.png", 'White background')
       concat content_tag(:span, ' | ')
-      concat link_to('SVG', @bucket.link("insignia/SVG/membership/#{membership}.svg"), disposition: 'inline')
+      concat dl_link('SVG', "insignia/SVG/membership/#{membership}.svg")
     end
   end
 
   def mm_insignia
     content_tag(:div, class: 'mm-insignia') do
-      concat image_tag(@bucket.link("insignia/PNG/mm.png"), height: 30)
+      concat image_tag(@bucket.link('insignia/PNG/mm.png'), height: 30)
       concat tag(:br)
-      concat link_to('PNG', @bucket.link("insignia/PNG/mm.png"), disposition: 'inline')
+      concat dl_link('SVG', 'insignia/PNG/mm.png')
       concat content_tag(:span, ' | ')
-      concat link_to('SVG', @bucket.link("insignia/SVG/mm.svg"), disposition: 'inline')
+      concat dl_link('SVG', 'insignia/SVG/mm.svg')
     end
+  end
+
+private
+
+  def dl_link(text, path, title = nil)
+    link_to(text, @bucket.link(path), disposition: 'inline', title: title)
   end
 end
