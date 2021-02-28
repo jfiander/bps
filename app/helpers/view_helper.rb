04444e6ebe0cbcbd1d924c3a7bf43ad62f4c5f94
Julian Fiander <jsfiander@gmail.com>
2021-02-28 12:09:43 -0500

    Fix event show view

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -28,13 +28,6 @@ module ViewHelper
     ).html_safe
   end
 
-  def render_both_tables(events, long_form: false)
-    # html_safe: Properly sanitized by Rails
-    %i[desktop mobile].map do |t|
-      render("event/#{t}/table", events: events, long_form: long_form)
-    end.join.html_safe
-  end
-
 private
 
   def office_rank(office)
