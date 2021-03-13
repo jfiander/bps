734b9489b0e33314dfab4302bba9c70f4830aa5c
Julian Fiander <jsfiander@gmail.com>
2021-03-13 14:23:10 -0500

    Fix nominations mailer previews

--- app/mailers/previews/nominations_mailer_preview.rb
+++ app/mailers/previews/nominations_mailer_preview.rb
@@ -3,15 +3,19 @@
 class NominationsMailerPreview < ApplicationMailerPreview
   def nomination_for_moose_milk
     NominationsMailer.nomination(
-      user, 'Bill Booth Moose Milk', 'John Q Public', 'Executive Committee'
+      user, 'Bill Booth Moose Milk', 'John Q Public', 'How they helped', 'Executive Committee'
     )
   end
 
   def nomination_for_education
-    NominationsMailer.nomination(user, 'Education', 'John Q Public', 'SEO')
+    NominationsMailer.nomination(
+      user, 'Education', 'John Q Public', 'Details of education', 'SEO'
+    )
   end
 
   def nomination_for_outstanding_service
-    NominationsMailer.nomination(user, 'Outstanding Service', 'John Q Public', 'Commander')
+    NominationsMailer.nomination(
+      user, 'Outstanding Service', 'John Q Public', 'Description of service', 'Commander'
+    )
   end
 end
