ba489c5fed602d927effa91d1b62bec1df1e96d0
Julian Fiander <jsfiander@gmail.com>
2021-04-11 02:05:11 -0400

    Disable SSL for VPC-internal requests

--- app/controllers/concerns/application/security.rb
+++ app/controllers/concerns/application/security.rb
@@ -18,7 +18,7 @@ module Application
   private
 
     def ssl_configured?
-      Rails.env.production? || Rails.env.staging?
+      (Rails.env.production? || Rails.env.staging?) && request.host !~ /.internal.bpsd9.org$/
     end
 
     def handle_unverified_request
