f2e6e1b37970ff0e917a89797b3a614e41cf7621
Julian Fiander <jsfiander@gmail.com>
2021-04-13 21:23:07 -0400

    Improve activity feed styling

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -6,12 +6,13 @@ module TimeHelper
   VERBOSE_DATE = '%a %d %b %Y'
 
   TIME_WITH_ZONE = "#{TIME} %Z"                            # 0000 EST
+  FULL_DATE = "#{SHORT_DATE}/%Y"                           # 1/1/2019
   FULL_TIME_FORMAT = "%A %d %B %Y at #{TIME_WITH_ZONE}"    # Tuesday 01 January 2019 at 0000 EST
   LONG_TIME_FORMAT = "#{VERBOSE_DATE} @ #{TIME_WITH_ZONE}" # Tue 01 Jan 2019 @ 0000 EST
-  MEDIUM_TIME_FORMAT = "#{SHORT_DATE}/%Y @ #{TIME}"        # 1/1/2019 @ 0000
+  MEDIUM_TIME_FORMAT = "#{FULL_DATE} @ #{TIME}"            # 1/1/2019 @ 0000
   SHORT_TIME_FORMAT = "#{SHORT_DATE} @ #{TIME}"            # 1/1 @ 0000
   EVENT_DATE_FORMAT = VERBOSE_DATE                         # Tue 01 Jan 2019
-  SIMPLE_DATE_FORMAT = "#{SHORT_DATE}/%Y"                  # 1/1/2019
+  SIMPLE_DATE_FORMAT = "#{FULL_DATE}"                      # 1/1/2019
   PUBLIC_DATE_FORMAT = '%b %-d'                            # Jan 1
   PUBLIC_TIME_FORMAT = '%l:%M %P %Z'                       # 12:00 am EST
   DURATION_FORMAT = '%-kh %Mm'                             # 0h 00m
