ba87aa5950e4c5b2bf51f792ea78a85e5a373004
Julian Fiander <jsfiander@gmail.com>
2021-05-12 00:18:17 -0400

    Add 406 error handler and support JSON error display

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
+  before_action :default_format
+
   def not_found
-    render status: 404
+    respond_to do |format|
+      format.html { render(status: 404) }
+      format.json { render(json: { error: 'Page not found.' }, status: 404) }
+    end
+  end
+
+  def not_acceptable
+    respond_to do |format|
+      format.html { render(status: 406) }
+      format.json { render(json: { error: 'Format not supported.' }, status: 406) }
+    end
   end
 
   def internal_server_error
-    render status: 500
+    respond_to do |format|
+      format.html { render(status: 500) }
+      format.json { render(json: { error: 'Internal server error.' }, status: 500) }
+    end
+  end
+
+private
+
+  def default_format
+    request.format = params[:format].in?(%w[json js]) ? 'json' : 'html'
   end
 end
