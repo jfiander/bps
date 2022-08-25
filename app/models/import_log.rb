d6b7d706a0f42686c4718a7d8550e43756d65045
Julian Fiander <jsfiander@gmail.com>
2022-08-25 00:01:49 -0400

    Add S3 log accessors
    S3 logs are written even in dryrun mode

--- app/models/import_log.rb
+++ app/models/import_log.rb
@@ -5,6 +5,23 @@ class ImportLog < ApplicationRecord
     all.order(:created_at).last
   end
 
+  # :nocov:
+  def self.archives
+    BPS::S3.new(:files).list('user_imports/').map do |f|
+      f.key.delete('user_imports/').delete('.proto').to_i
+    end.sort
+  end
+
+  def self.from_s3(timestamp)
+    bin = BPS::S3.new(:files).download("user_imports/#{timestamp}.proto")
+    new(proto: bin).decode
+  end
+
+  def self.latest_from_s3
+    from_s3(archives.last)
+  end
+  # :nocov:
+
   def decode
     BPS::Update::UserDataImport.decode(proto)
   end
