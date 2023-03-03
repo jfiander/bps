986fac5b094cc24138258682fc5ca898a94a84a4
Julian Fiander <jsfiander@gmail.com>
2023-03-03 16:59:52 -0500

    Improve logos
    Separate mobile and print headers
    Add dark mode logo
    Reorganize print header and footer styles
    Remove unused header link constructors
    Also support dark mode logos in mailers

--- app/controllers/concerns/application/layout_and_formatting.rb
+++ app/controllers/concerns/application/layout_and_formatting.rb
@@ -9,10 +9,10 @@ module Application
 
     def load_layout_images
       pick_header_image
-      @header_logo = BPS::S3.new(:static).link('logos/ABC/png/short/tr/slogan/300.png')
-      @print_logo = BPS::S3.new(:static).link('logos/ABC/png/long/tr/birmingham/2000.png')
-      @wheel_logo = BPS::S3.new(:static).link('flags/PNG/WHEEL.thumb.png')
-      @dca_award = BPS::S3.new(:static).link('logos/DCA_web_2016.png')
+      @logos = {
+        light: BPS::S3.new(:static).link('logos/ABC/png/light/long/tr/birmingham/2000.png'),
+        dark: BPS::S3.new(:static).link('logos/ABC/png/dark/desat/long/tr/birmingham/2000.png')
+      }
     end
 
     def pick_header_image
