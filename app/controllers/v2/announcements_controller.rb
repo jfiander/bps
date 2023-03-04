a9224d2c48395999d90d13f5b6c6de16ae4aab81
Julian Fiander <jsfiander@gmail.com>
2023-03-04 13:59:28 -0500

    Restructure controllers for v2

--- app/controllers/v2/announcements_controller.rb
+++ app/controllers/v2/announcements_controller.rb
@@ -1,77 +1,79 @@
 # frozen_string_literal: true
 
-class V2::AnnouncementsController < ApplicationController
-  secure!(:newsletter, only: %i[create destroy hide unhide])
+module V2
+  class AnnouncementsController < ApplicationController
+    secure!(:newsletter, only: %i[create destroy hide unhide])
 
-  before_action :load_announcement, only: %i[show destroy hide unhide]
+    before_action :load_announcement, only: %i[show destroy hide unhide]
 
-  def index
-    redirect_to(v2_bilges_path)
-  end
+    def index
+      redirect_to(v2_bilges_path)
+    end
 
-  def show
-    return announcement_not_found unless @announcement.present?
+    def show
+      return announcement_not_found if @announcement.blank?
 
-    send_announcement(@announcement)
-  end
+      send_announcement(@announcement)
+    end
 
-  def create
-    AnnouncementFile.create(
-      title: announcement_params[:title],
-      file: announcement_params[:file]
-    )
+    def create
+      AnnouncementFile.create(
+        title: announcement_params[:title],
+        file: announcement_params[:file]
+      )
 
-    redirect_to(
-      newsletter_path,
-      success: 'Announcement uploaded successfully.'
-    )
-  end
+      redirect_to(
+        newsletter_path,
+        success: 'Announcement uploaded successfully.'
+      )
+    end
 
-  def destroy
-    @announcement.destroy
+    def destroy
+      @announcement.destroy
 
-    redirect_to(v2_bilges_path, success: 'Announcement removed successfully.')
-  end
+      redirect_to(v2_bilges_path, success: 'Announcement removed successfully.')
+    end
 
-  def hide
-    @announcement.hide!
+    def hide
+      @announcement.hide!
 
-    redirect_to(v2_bilges_path, success: 'Announcement hidden successfully.')
-  end
+      redirect_to(v2_bilges_path, success: 'Announcement hidden successfully.')
+    end
 
-  def unhide
-    @announcement.unhide!
+    def unhide
+      @announcement.unhide!
 
-    redirect_to(v2_bilges_path, success: 'Announcement unhidden successfully.')
-  end
+      redirect_to(v2_bilges_path, success: 'Announcement unhidden successfully.')
+    end
 
-private
+  private
 
-  def announcement_params
-    params.permit(:id, :title, :file)
-  end
+    def announcement_params
+      params.permit(:id, :title, :file)
+    end
 
-  def load_announcement
-    @announcement = AnnouncementFile.find(announcement_params[:id])
-  end
+    def load_announcement
+      @announcement = AnnouncementFile.find(announcement_params[:id])
+    end
 
-  def announcement_not_found
-    error_redirect_to_newsletter('There was a problem accessing that announcement.')
-  end
+    def announcement_not_found
+      error_redirect_to_newsletter('There was a problem accessing that announcement.')
+    end
 
-  def send_announcement(file)
-    send_data(
-      URI.parse(BPS::S3.new(:files).link(file.file.s3_object.key)).open.read,
-      filename: "BPS Announcement - #{file.title}.pdf",
-      type: 'application/pdf',
-      disposition: 'inline'
-    )
-  rescue SocketError
-    error_redirect_to_newsletter('There was a problem accessing that announcement.')
-  end
+    def send_announcement(file)
+      send_data(
+        URI.parse(BPS::S3.new(:files).link(file.file.s3_object.key)).open.read,
+        filename: "BPS Announcement - #{file.title}.pdf",
+        type: 'application/pdf',
+        disposition: 'inline'
+      )
+    rescue SocketError
+      error_redirect_to_newsletter('There was a problem accessing that announcement.')
+    end
 
-  def error_redirect_to_newsletter(message)
-    flash[:alert] = message
-    redirect_to(newsletter_path)
+    def error_redirect_to_newsletter(message)
+      flash[:alert] = message
+      redirect_to(newsletter_path)
+    end
   end
 end
--- app/controllers/v2/bilges_controller.rb
+++ app/controllers/v2/bilges_controller.rb
@@ -1,105 +1,107 @@
 # frozen_string_literal: true
 
-class V2::BilgesController < ApplicationController
-  secure!(:newsletter, only: %i[create])
-  title!('The Bilge Chatter')
+module V2
+  class BilgesController < ApplicationController
+    secure!(:newsletter, only: %i[create])
+    title!('The Bilge Chatter')
 
-  def index
-    last_18 = BilgeFile.last_18
-    @bilges = user_signed_in? ? BilgeFile.ordered : last_18
-    @public_ids = last_18.map(&:id)
-    @issues = BilgeFile.issues
-    load_announcements
-  end
-
-  def bilge
-    bilge = pick_bilge
-    return bilge_not_found unless bilge.present?
-
-    send_bilge(bilge)
-  end
-
-  def upload
-    verb = update_bilge
-    issue = "#{bilge_params[:issue]['date(1i)']}/#{bilge_params[:issue]['date(2i)']}"
-
-    notify_for_bilge if bilge_params[:bilge_notify]
-    redirect_to(v2_bilges_path, success: "Bilge Chatter #{issue} #{verb} successfully.")
-  end
-
-private
-
-  def bilge_params
-    params.permit(
-      :id, :page_name, :save, :preview, :file, :bilge_remove, :bilge_notify,
-      issue: ['date(1i)', 'date(2i)']
-    )
-  end
-
-  def find_bilge_issue
-    BilgeFile.find_by(
-      year: bilge_params[:issue]['date(1i)'],
-      month: bilge_params[:issue]['date(2i)']
-    )
-  end
-
-  def load_announcements
-    @announcements = AnnouncementFile.ordered
-    @announcements = @announcements.visible unless current_user&.permitted?(:newsletter)
-  end
-
-  def update_bilge
-    if bilge_params[:bilge_remove].present?
-      find_bilge_issue&.destroy
-      'removed'
-    elsif (file = find_bilge_issue).present?
-      file.update(file: bilge_params[:file])
-      notify_for_bilge if bilge_params[:bilge_notify]
-      'replaced'
-    else
-      create_bilge
-      notify_for_bilge if bilge_params[:bilge_notify]
-      'uploaded'
+    def index
+      last_18 = BilgeFile.last_18
+      @bilges = user_signed_in? ? BilgeFile.ordered : last_18
+      @public_ids = last_18.map(&:id)
+      @issues = BilgeFile.issues
+      load_announcements
     end
-  end
 
-  def create_bilge
-    BilgeFile.create(
-      year: bilge_params[:issue]['date(1i)'],
-      month: bilge_params[:issue]['date(2i)'],
-      file: bilge_params[:file]
-    )
-  end
+    def bilge
+      bilge = pick_bilge
+      return bilge_not_found if bilge.blank?
 
-  def bilge_not_found
-    index
-    flash.now[:alert] = 'There was a problem accessing the Bilge Chatter.'
-    flash.now[:error] = 'Issue not found.'
-    render :index
-  end
+      send_bilge(bilge)
+    end
 
-  def send_bilge(bilge)
-    send_data(
-      URI.parse(BPS::S3.new(:bilge).link(bilge.file.s3_object.key)).open.read,
-      filename: "Bilge Chatter - #{bilge.full_issue}.pdf",
-      type: 'application/pdf',
-      disposition: 'inline'
-    )
-  rescue SocketError
-    newsletter
-    flash.now[:alert] = 'There was a problem accessing the Bilge Chatter. Please try again later.'
-    render :newsletter
-  end
+    def upload
+      verb = update_bilge
+      issue = "#{bilge_params[:issue]['date(1i)']}/#{bilge_params[:issue]['date(2i)']}"
 
-  def notify_for_bilge
-    editor = Committee.where(department: :secretary, name: 'Newsletter Editor')
-    membership = Committee.where(department: :administrative, name: 'Membership')
-    web = Committee.where(department: :secretary, name: 'Webmaster')
+      notify_for_bilge if bilge_params[:bilge_notify]
+      redirect_to(v2_bilges_path, success: "Bilge Chatter #{issue} #{verb} successfully.")
+    end
 
-    NotificationsMailer.bilge(
-      editor.or(membership).or(web).map { |c| c.user.email }.compact,
-      year: bilge_params[:issue]['date(1i)'],
-      month: bilge_params[:issue]['date(2i)']
-    ).deliver
+  private
+
+    def bilge_params
+      params.permit(
+        :id, :page_name, :save, :preview, :file, :bilge_remove, :bilge_notify,
+        issue: ['date(1i)', 'date(2i)']
+      )
+    end
+
+    def find_bilge_issue
+      BilgeFile.find_by(
+        year: bilge_params[:issue]['date(1i)'],
+        month: bilge_params[:issue]['date(2i)']
+      )
+    end
+
+    def load_announcements
+      @announcements = AnnouncementFile.ordered
+      @announcements = @announcements.visible unless current_user&.permitted?(:newsletter)
+    end
+
+    def update_bilge
+      if bilge_params[:bilge_remove].present?
+        find_bilge_issue&.destroy
+        'removed'
+      elsif (file = find_bilge_issue).present?
+        file.update(file: bilge_params[:file])
+        notify_for_bilge if bilge_params[:bilge_notify]
+        'replaced'
+      else
+        create_bilge
+        notify_for_bilge if bilge_params[:bilge_notify]
+        'uploaded'
+      end
+    end
+
+    def create_bilge
+      BilgeFile.create(
+        year: bilge_params[:issue]['date(1i)'],
+        month: bilge_params[:issue]['date(2i)'],
+        file: bilge_params[:file]
+      )
+    end
+
+    def bilge_not_found
+      index
+      flash.now[:alert] = 'There was a problem accessing the Bilge Chatter.'
+      flash.now[:error] = 'Issue not found.'
+      render :index
+    end
+
+    def send_bilge(bilge)
+      send_data(
+        URI.parse(BPS::S3.new(:bilge).link(bilge.file.s3_object.key)).open.read,
+        filename: "Bilge Chatter - #{bilge.full_issue}.pdf",
+        type: 'application/pdf',
+        disposition: 'inline'
+      )
+    rescue SocketError
+      newsletter
+      flash.now[:alert] = 'There was a problem accessing the Bilge Chatter. Please try again later.'
+      render :newsletter
+    end
+
+    def notify_for_bilge
+      editor = Committee.where(department: :secretary, name: 'Newsletter Editor')
+      membership = Committee.where(department: :administrative, name: 'Membership')
+      web = Committee.where(department: :secretary, name: 'Webmaster')
+
+      NotificationsMailer.bilge(
+        editor.or(membership).or(web).map { |c| c.user.email }.compact,
+        year: bilge_params[:issue]['date(1i)'],
+        month: bilge_params[:issue]['date(2i)']
+      ).deliver
+    end
   end
 end
