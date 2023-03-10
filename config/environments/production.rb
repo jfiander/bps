jfiander/bps

commit fd131a541569f47060a6f9763f3b6ed0e50123e3

Bump Rails to 6.1.7.2

config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.

Gemfile
@@ -8,7 +8,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'rails',               '6.0.0'
+gem 'rails',               '>= 6.1.7.2'
 
 gem 'puma',                '~> 4.3'
 
@@ -103,7 +103,7 @@ group :development, :test do
   gem 'factory_bot_rails',        '~> 4.8.2'
   gem 'fuubar',                   '~> 2.3.2'
   gem 'rails-controller-testing', '~> 1.0.4'
-  gem 'rspec-rails',              '~> 3.7.1'
+  gem 'rspec-rails',              '~> 3.9'
   gem 'simplecov',                '~> 0.15.1'
 
   # Rubocop

app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  force_ssl if: :ssl_configured?
   protect_from_forgery with: :exception
   add_flash_types :success, :error
 

app/controllers/concerns/application/security.rb
@@ -17,10 +17,6 @@ module Application
 
   private
 
-    def ssl_configured?
-      BPS::Application.deployed? && request.host !~ /.#{ENV['INTERNAL_DOMAIN']}$/
-    end
-
     def handle_unverified_request
       flash[:alert] = 'Sorry, please try that again.'
       redirect_to :back

config/application.rb
@@ -28,8 +28,6 @@ module BPS
     config.active_record.default_timezone = :local
     config.active_record.time_zone_aware_types = %i[datetime time]
 
-    config.active_record.sqlite3.represent_boolean_as_integer = true
-
     config.to_prepare { Devise::Mailer.layout('mailer') }
 
     # Ensure working temp dir exists

commit 944afc35aa0236860bbf22a647eb978e02cb2c8a

Fix ssl config
Force in Staging environment
Correctly exclude internal domain requests
  Was broken by fd131a54

config/environments/production.rb
@@ -49,6 +49,11 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = {
+    redirect: {
+      exclude: ->(request) { request.host =~ /\.#{ENV['INTERNAL_DOMAIN']}$/ }
+    }
+  }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.

config/environments/staging.rb
@@ -48,7 +48,12 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = {
+    redirect: {
+      exclude: ->(request) { request.host =~ /\.#{ENV['INTERNAL_DOMAIN']}$/ }
+    }
+  }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.