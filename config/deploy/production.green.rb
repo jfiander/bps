2b388a061d70c17986d8a9c0b5d32708c680bb10
Julian Fiander <jsfiander@gmail.com>
2023-03-31 16:54:34 -0400

    Add Green production deploy environment

---
+++ config/deploy/production.green.rb
@@ -0,0 +1,17 @@
+production_green_instance = 'green.production.internal.bpsd9.org'
+
+set :rvm_ruby_version, '2.7.4'
+
+role :app, "deploy@#{production_green_instance}"
+role :web, "deploy@#{production_green_instance}"
+role :db,  "deploy@#{production_green_instance}"
+
+set :environment, 'production'
+set :rails_env, 'production'
+set :branch, ENV.fetch('BRANCH', 'master')
+
+set :ssh_options, {
+  keys: %w(~/.ssh/bpsd9-jfiander.pem ~/.ssh/bpsd9.pem),
+  forward_agent: false,
+  auth_methods: %w(publickey)
+}
