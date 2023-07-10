2a8f53d231e04068bc266a191d0469c13655777f
Julian Fiander <jsfiander@gmail.com>
2023-07-09 23:53:32 -0400

    Isolate internal helper methods from nav structure

--- app/helpers/admin_menu_helper.rb
+++ app/helpers/admin_menu_helper.rb
@@ -50,9 +50,9 @@ module AdminMenuHelper
   end
 
   def show_link?(*roles, strict: false, **options)
-    req_cont, req_act, not_cont, not_act = link_requirements(options)
+    req_cont, req_act, not_cont, not_act = H.link_requirements(options)
     return false unless current_user&.permitted?(roles, strict: strict)
-    return false if invalid?(req_cont, req_act, not_cont, not_act)
+    return false if H.invalid?(req_cont, req_act, not_cont, not_act, controller)
 
     true
   end
@@ -174,60 +174,65 @@ private
     content_tag(:div, '', class: 'sidenav-spacer')
   end
 
-  def link_requirements(options)
-    req_cont = *options[:controller] || []
-    req_action = *options[:action] || []
-    not_cont = *options[:not_controller] || []
-    not_action = *options[:not_action] || []
+  # Internal helper methods
+  module H
+    class << self
+      def link_requirements(options)
+        req_cont = *options[:controller] || []
+        req_action = *options[:action] || []
+        not_cont = *options[:not_controller] || []
+        not_action = *options[:not_action] || []
 
-    [req_cont, req_action, not_cont, not_action]
-  end
+        [req_cont, req_action, not_cont, not_action]
+      end
 
-  def invalid?(req_cont, req_act, not_cont, not_act)
-    invalid_controller?(req_cont, req_act, not_cont, not_act) ||
-      invalid_action?(req_cont, req_act, not_cont, not_act) ||
-      invalid_combination?(req_cont, req_act, not_cont, not_act)
-  end
+      def invalid?(req_cont, req_act, not_cont, not_act, controller)
+        @controller = controller
 
-  def invalid_controller?(req_cont, req_act, not_cont, not_act)
-    (missing_controller?(req_cont) && req_act.blank?) ||
-      (wrong_controller?(not_cont) && not_act.blank?)
-  end
+        invalid_controller?(req_cont, req_act, not_cont, not_act) ||
+          invalid_action?(req_cont, req_act, not_cont, not_act) ||
+          invalid_combination?(req_cont, req_act, not_cont, not_act)
+      end
 
-  def invalid_action?(req_cont, req_act, not_cont, not_act)
-    (missing_action?(req_act) && req_cont.blank?) || (wrong_action?(not_act) && not_cont.blank?)
-  end
+    private
 
-  def invalid_combination?(req_cont, req_act, not_cont, not_act)
-    (missing_controller?(req_cont) && missing_action?(req_act)) ||
-      (wrong_controller?(not_cont) && wrong_action?(not_act))
-  end
+      def invalid_controller?(req_cont, req_act, not_cont, not_act)
+        (missing_controller?(req_cont) && req_act.blank?) ||
+          (wrong_controller?(not_cont) && not_act.blank?)
+      end
 
-  def missing_controller?(req_controller = nil)
-    return false if req_controller.blank?
+      def invalid_action?(req_cont, req_act, not_cont, not_act)
+        (missing_action?(req_act) && req_cont.blank?) || (wrong_action?(not_act) && not_cont.blank?)
+      end
 
-    !controller.controller_name.in?(req_controller)
-  end
+      def invalid_combination?(req_cont, req_act, not_cont, not_act)
+        (missing_controller?(req_cont) && missing_action?(req_act)) ||
+          (wrong_controller?(not_cont) && wrong_action?(not_act))
+      end
 
-  def wrong_controller?(not_controller = nil)
-    return false if not_controller.blank?
+      def missing_controller?(req_controller = nil)
+        return false if req_controller.blank?
 
-    controller.controller_name.in?(not_controller)
-  end
+        !@controller.controller_name.in?(req_controller)
+      end
 
-  def missing_action?(req_action = nil)
-    return false if req_action.blank?
+      def wrong_controller?(not_controller = nil)
+        return false if not_controller.blank?
 
-    !controller.action_name.in?(req_action)
-  end
+        @controller.controller_name.in?(not_controller)
+      end
 
-  def wrong_action?(not_action = nil)
-    return false if not_action.blank?
+      def missing_action?(req_action = nil)
+        return false if req_action.blank?
 
-    controller.action_name.in?(not_action)
-  end
+        !@controller.action_name.in?(req_action)
+      end
 
-  def default_url_options
-    Rails.application.config.active_job.default_url_options
+      def wrong_action?(not_action = nil)
+        return false if not_action.blank?
+
+        @controller.action_name.in?(not_action)
+      end
+    end
   end
 end
