490a16fde0fa2a4881e7d4ef914df23bdf87032f
Julian Fiander <jsfiander@gmail.com>
2023-07-12 00:32:22 -0400

    Remove errant safe nav

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -67,7 +67,7 @@ private
 
   def propagate_defaults!(yaml)
     yaml.each do |_menu, data|
-      menu_defaults = data&.slice(*DEFAULTABLE_FIELDS).reverse_merge(display: true)
+      menu_defaults = data.slice(*DEFAULTABLE_FIELDS).reverse_merge(display: true)
       data[:items].each do |item|
         item.reverse_merge!(menu_defaults)
         next unless item.key?(:children)
