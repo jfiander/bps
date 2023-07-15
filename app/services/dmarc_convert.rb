848be2d42281746a61837ce294ebd87b5cee4228
Julian Fiander <jsfiander@gmail.com>
2023-07-15 17:23:37 -0400

    Improve parsing helper methods name and separation
    Also remove unused default argument
    Rename reason method

--- app/services/dmarc_convert.rb
+++ app/services/dmarc_convert.rb
@@ -10,7 +10,7 @@ class DmarcConvert
     feedback = noko.children.first
 
     Dmarc::Feedback.new(
-      version: value(feedback, :version),
+      version: string(feedback, :version),
       report_metadata: report_metadata(child(feedback, :report_metadata)),
       policy_published: policy_published(child(feedback, :policy_published)),
       records: records(children(feedback, :record))
@@ -23,28 +23,28 @@ private
     date_range = child(data, :date_range)
 
     {
-      org_name: value(data, :org_name),
-      email: value(data, :email),
-      extra_contact_info: value(data, :extra_contact_info),
-      report_id: value(data, :report_id),
+      org_name: string(data, :org_name),
+      email: string(data, :email),
+      extra_contact_info: string(data, :extra_contact_info),
+      report_id: string(data, :report_id),
       date_range: {
-        begin: value(date_range, :begin).to_i,
-        end: value(date_range, :end).to_i
+        begin: integer(date_range, :begin),
+        end: integer(date_range, :end)
       },
-      error: value(data, :error)
+      error: string(data, :error)
     }
   end
 
   def policy_published(data)
     {
-      domain: value(data, :domain),
+      domain: string(data, :domain),
       adkim: enum(data, :adkim),
       aspf: enum(data, :aspf),
       p: enum(data, :p),
       sp: enum(data, :sp),
-      pct: value(data, :pct).to_i,
+      pct: integer(data, :pct),
       np: enum(data, :np, optional: true),
-      fo: value(data, :fo)
+      fo: string(data, :fo)
     }
   end
 
@@ -61,13 +61,13 @@ private
   def record(row, auth_results, identifiers)
     {
       row: {
-        source_ip: value(row, :source_ip),
-        count: value(row, :count).to_i,
+        source_ip: string(row, :source_ip),
+        count: integer(row, :count),
         policy_evaluated: policy_evaluated(child(row, :policy_evaluated))
       },
       identifiers: {
-        header_from: value(identifiers, :header_from),
-        envelope_from: value(identifiers, :envelope_from)
+        header_from: string(identifiers, :header_from),
+        envelope_from: string(identifiers, :envelope_from)
       },
       auth_results: {
         dkim: dkim(children(auth_results, :dkim)),
@@ -77,30 +77,30 @@ private
   end
 
   def policy_evaluated(data)
-    reason = child(data, :reason)
+    reason_data = child(data, :reason)
 
     {
       disposition: enum(data, :disposition),
       dkim: enum(data, :dkim),
       spf: enum(data, :spf),
-      reason: (reason_data(reason) unless reason.nil?)
+      reason: (reason(reason_data) unless reason_data.nil?)
     }
   end
 
-  def reason_data(data)
+  def reason(data)
     {
       type: enum(data, :type),
-      comment: value(data, :comment)
+      comment: string(data, :comment)
     }
   end
 
   def dkim(data)
     data.map do |d|
       {
-        domain: value(d, :domain),
+        domain: string(d, :domain),
         result: enum(d, :result),
-        selector: value(d, :selector),
-        human_result: value(d, :human_result)
+        selector: string(d, :selector),
+        human_result: string(d, :human_result)
       }
     end
   end
@@ -108,12 +108,14 @@ private
   def spf(data)
     data.map do |s|
       {
-        domain: value(s, :domain),
+        domain: string(s, :domain),
         result: enum(s, :result),
         scope: enum(s, :scope, optional: true)
       }
     end
   end
+
+  ### Parsing Helpers ###
 
   def child(node, name)
     node.children.find { |c| c.name == name.to_s }
@@ -123,16 +125,20 @@ private
     node.children.select { |c| c.name == name.to_s }
   end
 
-  def value(node, name, default = '')
+  def string(node, name)
     c = child(node, name)
     return c.value if c.respond_to?(:value)
 
-    c&.children&.first&.to_s || default
+    c&.children&.first&.to_s
+  end
+
+  def integer(node, name)
+    string(node, name).to_i
   end
 
   def enum(node, name, optional: false)
     return if child(node, name).nil? && optional
 
-    value(node, name).upcase.to_sym
+    string(node, name).upcase.to_sym
   end
 end
