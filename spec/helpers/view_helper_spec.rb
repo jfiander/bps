e3441a7f1042a4180e13aa6c5347c460c35648e7
Julian Fiander <jsfiander@gmail.com>
2024-03-15 22:02:58 -0400

    Update view helper spec to match SVGs on S3

--- spec/helpers/view_helper_spec.rb
+++ spec/helpers/view_helper_spec.rb
@@ -8,18 +8,17 @@ RSpec.describe ViewHelper do
       expect(described_class.officer_flag('educational')).to include(
         '<svg ',
         '<title>LTC</title>',
-        '<path d="M 0 0 l 384 0 l 0 256 l -384 0 l 0 -256 z" fill="#E4002B" stroke="none" />',
-        '<g transform="translate(-64)" stroke="none">'
+        '<path d="M 0 0 l 3072 0 l 0 2048 l -3072 0 l 0 -2048 z" fill="#E4002B" />',
+        '<g transform="translate(-512)">'
       )
     end
 
     it 'gets the correct SVG for Cdr' do
-      described_class.officer_flag('commander').inspect
       expect(described_class.officer_flag('commander')).to include(
         '<svg ',
         '<title>CDR</title>',
-        '<path d="M 0 0 l 384 0 l 0 256 l -384 0 l 0 -256 z" fill="#012169" stroke="none" />',
-        '<g transform="translate(-96, 24)" stroke="none">'
+        '<path d="M 0 0 l 3072 0 l 0 2048 l -3072 0 l 0 -2048 z" fill="#012169" />',
+        '<g transform="translate(-768, 192)">'
       )
     end
 
@@ -27,8 +26,8 @@ RSpec.describe ViewHelper do
       expect(described_class.officer_flag('asst_secretary')).to include(
         '<svg ',
         '<title>1LT</title>',
-        '<path d="M 0 0 l 384 0 l 0 256 l -384 0 l 0 -256 z" fill="#FFFFFF" ' \
-        'stroke="#000000" stroke-width="1" />'
+        '<path d="M 0 0 l 3072 0 l 0 2048 l -3072 0 l 0 -2048 z" fill="#FFFFFF" ' \
+        'stroke="#000000" stroke-width="5" />'
       )
     end
   end
