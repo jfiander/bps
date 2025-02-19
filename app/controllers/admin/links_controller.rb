da034ad18250f6a9943e9811c9e3e9bb1630ab1e
Julian Fiander <jsfiander@gmail.com>
2025-02-19 01:43:22 -0500

    Add CEN slides download

--- app/controllers/admin/links_controller.rb
+++ app/controllers/admin/links_controller.rb
@@ -26,6 +26,7 @@ module Admin
         'abc-2021' => seo_link('Courses/ABC3-2021.zip'),
         'abc' => seo_link('Courses/ABC3-16-hour.zip'),
         'abc-8hr' => seo_link('Courses/ABC3-8-hour.zip'),
+        'cen-2025' => seo_link('Courses/CEN/CEN_2025_slides.zip'),
         'Sextant Lease (fillable)' => seo_link('Sextant/fillable/SEXTANT_LEASE_AGREEMENT.pdf'),
         'Sextant Lease (flat)' => seo_link('Sextant/SEXTANT_LEASE_AGREEMENT.pdf')
       }
