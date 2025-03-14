03d13ae4ec838017122b54061815346a76122de1
Julian Fiander <jsfiander@gmail.com>
2025-03-14 16:50:21 -0400

    Tweak CORS regexes

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -4,9 +4,9 @@ if defined? Rack::Cors
   Rails.configuration.middleware.insert_before 0, Rack::Cors do
     allow do
       origins [
-        %r{\Ahttps?://(.*?)\.bpsd9.org(:\d+)?\z},
+        %r{\Ahttps?://(.*?)\.bpsd9\.org(:\d+)?\z},
         %r{\Ahttps?://#{ENV['DOMAIN']}\z},
-        %r{\Ahttps?://assets.#{ENV['DOMAIN']}\z}
+        %r{\Ahttps?://assets\.#{ENV['DOMAIN']}\z}
       ]
       resource '/assets/*'
       resource '/webfonts/*'
